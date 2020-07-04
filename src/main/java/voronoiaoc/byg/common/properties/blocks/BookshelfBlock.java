721fd30e6c61de4c5e40d3cc22f6e9f87044be09
Linguardium <Linguardium@hobbyist.dev>
2020-07-04 16:37:13 -0700

    fix bookshelves with enchanting tables
    use c:bookshelves tag for bookshelves to allow others to add their own to c:bookshelves for compatibility
    
    add byg:bookshelves to the c:bookshelves tag to allow byg bookshelves to be used in other instances where bookshelf mods use tags

--- src/main/java/voronoiaoc/byg/common/properties/blocks/BookshelfBlock.java
+++ src/main/java/voronoiaoc/byg/common/properties/blocks/BookshelfBlock.java
@@ -1,9 +1,12 @@
 package voronoiaoc.byg.common.properties.blocks;
 
+import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.block.Block;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
 
 public class BookshelfBlock extends Block {
-
+    public static Tag<Block> BOOKSHELF_TAG = TagRegistry.block(new Identifier("c","bookshelves"));
     protected BookshelfBlock(Settings builder) {
         super(builder);
     }
