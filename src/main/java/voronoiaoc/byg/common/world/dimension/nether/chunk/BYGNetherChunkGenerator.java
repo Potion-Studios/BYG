5753c27322baef3126b9baa4f938bc617aed1941
CorgiTaco <>
2020-07-04 05:20:03 -0700

    Immersive portals?

--- src/main/java/voronoiaoc/byg/common/world/dimension/nether/chunk/BYGNetherChunkGenerator.java
+++ src/main/java/voronoiaoc/byg/common/world/dimension/nether/chunk/BYGNetherChunkGenerator.java
@@ -6,14 +6,20 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.provider.BiomeProvider;
+import net.minecraft.world.chunk.IChunk;
 import net.minecraft.world.gen.feature.Feature;
+import net.minecraftforge.fml.ModList;
 import voronoiaoc.byg.common.world.worldtype.chunk.simplex.Simplex3DNoiseChunkGenerator;
 import voronoiaoc.byg.config.BYGWorldConfig;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
 public class BYGNetherChunkGenerator extends Simplex3DNoiseChunkGenerator<BYGNetherGenSettings> {
     public final double[] constHeightThresholds = this.generateConstHeightThresholds();
+
+    private final BYGNetherGenSettings genSettings;
 
     public static int netherHeight() {
         return (BYGWorldConfig.tallNether.get() ? 256 : 128);
@@ -21,6 +27,7 @@ public class BYGNetherChunkGenerator extends Simplex3DNoiseChunkGenerator<BYGNet
 
     public BYGNetherChunkGenerator(World world, BiomeProvider provider, BYGNetherGenSettings settings) {
         super(world, provider, 4, netherHeight(), settings, 4, 2, 684.412, 2053.236, 8.555150000000001, 34.2206);
+        this.genSettings = settings;
     }
 
     protected double[] getColumnBiomeParams(int biomeParam1, int biomeParam2) {
@@ -78,6 +85,69 @@ public class BYGNetherChunkGenerator extends Simplex3DNoiseChunkGenerator<BYGNet
         }
 
         return super.getPossibleCreatures(creatureType, pos);
+    }
+
+    @Override
+    protected void makeBedrock(IChunk chunkIn, Random rand) {
+        BlockPos.Mutable blockpos$mutable = new BlockPos.Mutable();
+        int i = chunkIn.getPos().getXStart();
+        int j = chunkIn.getPos().getZStart();
+        int k = genSettings.getBedrockFloorHeight();
+        int l = genSettings.getBedrockRoofHeight();
+        Iterator var9 = BlockPos.getAllInBoxMutable(i, 0, j, i + 15, 0, j + 15).iterator();
+
+        if (ModList.get().isLoaded("immersive_portals")) {
+            while (true) {
+                BlockPos blockpos;
+                int j1;
+                do {
+                    if (!var9.hasNext()) {
+                        return;
+                    }
+
+                    blockpos = (BlockPos) var9.next();
+                    if (l > 0) {
+                        for (j1 = l; j1 >= l - 4; --j1) {
+                            if (j1 >= l - rand.nextInt(5)) {
+                                chunkIn.setBlockState(blockpos$mutable.setPos(blockpos.getX(), j1, blockpos.getZ()), Blocks.OBSIDIAN.getDefaultState(), false);
+                            }
+                        }
+                    }
+                } while (k >= 256);
+
+                for (j1 = k + 4; j1 >= k; --j1) {
+                    if (j1 <= k + rand.nextInt(5)) {
+                        chunkIn.setBlockState(blockpos$mutable.setPos(blockpos.getX(), j1, blockpos.getZ()), Blocks.OBSIDIAN.getDefaultState(), false);
+                    }
+                }
+            }
+        }
+        else {
+            while (true) {
+                BlockPos blockpos;
+                int j1;
+                do {
+                    if (!var9.hasNext()) {
+                        return;
+                    }
+
+                    blockpos = (BlockPos) var9.next();
+                    if (l > 0) {
+                        for (j1 = l; j1 >= l - 4; --j1) {
+                            if (j1 >= l - rand.nextInt(5)) {
+                                chunkIn.setBlockState(blockpos$mutable.setPos(blockpos.getX(), j1, blockpos.getZ()), Blocks.BEDROCK.getDefaultState(), false);
+                            }
+                        }
+                    }
+                } while (k >= 256);
+
+                for (j1 = k + 4; j1 >= k; --j1) {
+                    if (j1 <= k + rand.nextInt(5)) {
+                        chunkIn.setBlockState(blockpos$mutable.setPos(blockpos.getX(), j1, blockpos.getZ()), Blocks.BEDROCK.getDefaultState(), false);
+                    }
+                }
+            }
+        }
     }
 
     public int getGroundHeight() {
