47fae8f28b9553e9a7365a4a13b19d5eccb5366e
CorgiTaco <>
2020-07-06 16:09:20 -0700

    Fix weight manager config.

--- src/main/java/voronoiaoc/byg/BYG.java
+++ src/main/java/voronoiaoc/byg/BYG.java
@@ -57,7 +57,6 @@ public class BYG {
     public BYG() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, BYGConfig.COMMON_CONFIG);
         BYGWorldConfig.loadConfig(BYGWorldConfig.COMMON_CONFIG, FMLPaths.CONFIGDIR.get().resolve(MOD_ID + "-world-common.toml"));
-        ConfigWeightManager.loadConfig(ConfigWeightManager.COMMON_CONFIG, FMLPaths.CONFIGDIR.get().resolve(BYG.MOD_ID + "-weights-common.toml"));
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::bygCommonSetup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::bygClientSetup);
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::bygLoadComplete);
--- src/main/java/voronoiaoc/byg/common/properties/BYGBlockProperties.java
+++ src/main/java/voronoiaoc/byg/common/properties/BYGBlockProperties.java
@@ -226,7 +226,6 @@ public class BYGBlockProperties {
                     .hardnessAndResistance(0.0f)
                     .notSolid()
                     .doesNotBlockMovement()
-
             );
             setRegistryName(registryName);
         }
