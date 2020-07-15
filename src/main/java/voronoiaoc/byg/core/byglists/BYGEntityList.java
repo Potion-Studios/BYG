a1e294ba0632ec1ca1ab4fa02885354ab60ed223
Linguardium <Linguardium@hobbyist.dev>
2020-07-15 04:59:14 -0700

    fix "no data fixer for entity" error in boats
    also fixed item group naming in English lang file

--- src/main/java/voronoiaoc/byg/core/byglists/BYGEntityList.java
+++ src/main/java/voronoiaoc/byg/core/byglists/BYGEntityList.java
@@ -1,10 +1,12 @@
 package voronoiaoc.byg.core.byglists;
 
+import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
+import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
 import voronoiaoc.byg.BYG;
 import voronoiaoc.byg.common.entity.boat.BYGBoatEntity;
 
 public class BYGEntityList {
-    public static final EntityType<BYGBoatEntity> BYGBOAT = EntityType.Builder.<BYGBoatEntity>create(BYGBoatEntity::new, SpawnGroup.MISC).setDimensions(1.375F, 0.5625F).build(BYG.MODID + ":boat");
+    public static final EntityType<BYGBoatEntity> BYGBOAT = FabricEntityTypeBuilder.<BYGBoatEntity>create(SpawnGroup.MISC,BYGBoatEntity::new).dimensions(new EntityDimensions(1.375F, 0.5625F,false)).build();
 }
