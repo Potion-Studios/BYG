4f3ca85ac942e4752791d173a5e11fcb77f9560a
CorgiTaco <>
2020-08-02 23:26:39 -0700

    fix y levels

--- src/main/java/voronoiaoc/byg/mixin/MixinSlimeEntity.java
+++ src/main/java/voronoiaoc/byg/mixin/MixinSlimeEntity.java
@@ -8,7 +8,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.Difficulty;
 import net.minecraft.world.IWorld;
 import net.minecraft.world.biome.Biome;
-import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.biome.Biomes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,17 +18,11 @@ import java.util.Random;
 
 @Mixin(SlimeEntity.class)
 public class MixinSlimeEntity {
-
-
-
-
     @Inject(at = @At("HEAD"), method = "func_223366_c(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/IWorld;Lnet/minecraft/entity/SpawnReason;Lnet/minecraft/util/math/BlockPos;Ljava/util/Random;)Z", cancellable = true)
     private static void injectSwampCategory(EntityType<SlimeEntity> entity, IWorld world, SpawnReason reason, BlockPos spawnPos, Random randomIn, CallbackInfoReturnable<Boolean> cir) {
         if (world.getDifficulty() != Difficulty.PEACEFUL) {
             Biome biome = world.getBiome(spawnPos);
-            int bottomY =  world.getHeight(Heightmap.Type.OCEAN_FLOOR, spawnPos.getX(), spawnPos.getZ());
-
-            if (biome.getCategory() == Biome.Category.SWAMP && spawnPos.getY() > bottomY && spawnPos.getY() < bottomY + 20 && randomIn.nextFloat() < 0.5F && randomIn.nextFloat() < world.getCurrentMoonPhaseFactor() && world.getLight(spawnPos) <= randomIn.nextInt(8)) {
+            if (biome.getCategory() == Biome.Category.SWAMP && biome != Biomes.SWAMP && spawnPos.getY() > 50 && spawnPos.getY() < 70 && randomIn.nextFloat() < 0.5F && randomIn.nextFloat() < world.getCurrentMoonPhaseFactor() && world.getLight(spawnPos) <= randomIn.nextInt(8)) {
                cir.setReturnValue(MobEntity.canSpawnOn(entity, world, reason, spawnPos, randomIn));
             }
         }
