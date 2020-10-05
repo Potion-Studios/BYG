67dc15b17b772b8054bb7ed344ae373f914c8c55
AOCAWOL <51396466+AOCAWOL@users.noreply.github.com>
2020-10-04 20:21:30 -0700

    -nether biomes part 1

--- src/main/java/voronoiaoc/byg/core/byglists/BYGBiomeList.java
+++ src/main/java/voronoiaoc/byg/core/byglists/BYGBiomeList.java
@@ -180,7 +180,7 @@ public class BYGBiomeList {
     public static final Biome SNOWYDECIDUOUSFORESTHILLS = new SnowyDeciduousForestHills();
     public static final Biome TROPICALFUNGALRAINFORESTHILLS = new TropicalFungalRainforestHills();
     public static final Biome TROPICALRAINFORESTHILLS = new TropicalRainForestHills();
-    //    public static final Biome VOLCANO = new VolcanoFeature();
+    //public static final Biome VOLCANO = new VolcanoFeature();
     public static final Biome ZELKOVAFORESTHILLS = new ZelkovaForestHills();
 
     //Nether
