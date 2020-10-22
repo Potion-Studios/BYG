fb31ab4abb99af64c1258db4d80713c7c7ca5625
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2020-10-22 13:03:55 -0700

    Fix Cypress Beach, use the layer random to get weighted Entries... See LayerRandomWeightedListUtil
    Signed-off-by: CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>

---
+++ src/main/java/corgiaoc/byg/util/LayerRandomWeightedListUtil.java
@@ -0,0 +1,24 @@
+package corgiaoc.byg.util;
+
+import net.minecraft.util.WeightedList;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.INoiseRandom;
+
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class LayerRandomWeightedListUtil {
+
+    public static Biome getBiome(WeightedList<Biome> biomeWeightedList, INoiseRandom layerNoise) {
+        return getShuffledStream(biomeWeightedList, layerNoise).findFirst().orElseThrow(RuntimeException::new);
+    }
+
+    private static Stream<Biome> getShuffledStream(WeightedList<Biome> biomeWeightedList, INoiseRandom layerNoise) {
+        biomeWeightedList.field_220658_a.forEach((biomeEntry) -> {
+            biomeEntry.field_220653_d = (float) -Math.pow((double)layerNoise.random(101) * 0.01F, 1.0F / (float)biomeEntry.field_220652_c);
+
+        });
+        biomeWeightedList.field_220658_a.sort(Comparator.comparingDouble((biomeEntry) -> biomeEntry.field_220653_d));
+        return biomeWeightedList.field_220658_a.stream().map(WeightedList.Entry::func_220647_b);
+    }
+}
