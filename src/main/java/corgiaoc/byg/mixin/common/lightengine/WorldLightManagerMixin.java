2eca085f363a517a40a2476d9e4600292b415e15
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-01-24 20:44:32 -0800

    -Light manager fix(Closes #260) taken from AE2 here(w/ permission): https://github.com/AppliedEnergistics/Applied-Energistics-2/pull/4935

---
+++ src/main/java/corgiaoc/byg/mixin/common/lightengine/WorldLightManagerMixin.java
@@ -0,0 +1,33 @@
+package corgiaoc.byg.mixin.common.lightengine;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.lighting.WorldLightManager;
+
+/**
+ * This fixes a bug in the Minecraft light-update code that runs after world-generation for
+ * {@link net.minecraft.world.chunk.ChunkPrimer}. If a chunk-section contains a light-emitting block, and we clear the
+ * entire chunk-section (i.e. as part of meteorite worldgen), the lighting-update will assume that the chunk section
+ * exists when it runs through {@link WorldLightManager#onBlockEmissionIncrease(net.minecraft.util.math.BlockPos, int)},
+ * even though the light-level is now 0 for the block.
+ * <p/>
+ * This mixin will cancel the now useless block-update and prevent the crash from occurring.
+ * <p/>
+ * See: https://github.com/AppliedEnergistics/Applied-Energistics-2/issues/4891
+ *
+ * Code taken from the following PR in Applied Energistics 2 w/ allowed permission(clarified here aswell): https://github.com/AppliedEnergistics/Applied-Energistics-2/pull/4935
+ *
+ */
+@Mixin(WorldLightManager.class)
+public class WorldLightManagerMixin {
+    @Inject(method = "onBlockEmissionIncrease", at = @At("HEAD"), cancellable = true)
+    public void onBlockEmissionIncrease(BlockPos blockPos, int lightLevel, CallbackInfo ci) {
+        if (lightLevel == 0) {
+            ci.cancel();
+        }
+    }
+}
