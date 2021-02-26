f2c41c4fb7075dde9c2dd8e39614e20588f0c0b7
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2021-02-25 17:26:17 -0800

    Git ignore

--- src/main/java/corgiaoc/byg/common/world/biome/overworld/Meadow.java
+++ src/main/java/corgiaoc/byg/common/world/biome/overworld/Meadow.java
@@ -5,11 +5,13 @@ import corgiaoc.byg.common.world.biome.BYGDefaultBiomeFeatures;
 import corgiaoc.byg.common.world.biome.BiomeUtil;
 import corgiaoc.byg.core.BYGBlocks;
 import corgiaoc.byg.core.world.BYGBiomes;
+import corgiaoc.byg.core.world.BYGConfiguredFeatures;
 import corgiaoc.byg.core.world.util.WorldGenRegistrationHelper;
 import net.minecraft.entity.EntityClassification;
 import net.minecraft.entity.EntityType;
 import net.minecraft.util.WeightedList;
 import net.minecraft.world.biome.*;
+import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.structure.StructureFeatures;
 import net.minecraft.world.gen.surfacebuilders.ConfiguredSurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
@@ -66,6 +68,7 @@ public class Meadow extends BYGBiome {
     }
 
     static {
+//        GENERATION_SETTINGS.withFeature(GenerationStage.Decoration.RAW_GENERATION, BYGConfiguredFeatures.ARCH);
         GENERATION_SETTINGS.withStructure(StructureFeatures.VILLAGE_PLAINS); //Plains Village
         GENERATION_SETTINGS.withStructure(StructureFeatures.PILLAGER_OUTPOST); //Pillager Outpost
         GENERATION_SETTINGS.withStructure(StructureFeatures.RUINED_PORTAL); //Ruined Portal Standard
