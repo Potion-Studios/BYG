79dd66e3bf20057ae6e2b7015b64e3b683079a78
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-03-07 18:06:24 -0800

    Threadlocal SurfaceContext for obtaining world.

--- src/main/java/corgiaoc/byg/common/world/surfacebuilder/CanyonSurfaceBuilder.java
+++ src/main/java/corgiaoc/byg/common/world/surfacebuilder/CanyonSurfaceBuilder.java
@@ -1,18 +1,18 @@
 package corgiaoc.byg.common.world.surfacebuilder;
 
-import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
 import corgiaoc.byg.BYG;
+import corgiaoc.byg.common.world.util.SurfaceContext;
 import corgiaoc.byg.core.BYGBlocks;
 import corgiaoc.byg.util.noise.fastnoise.FastNoise;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.IChunk;
+import net.minecraft.world.gen.ChunkGenerator;
 import net.minecraft.world.gen.Heightmap;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
@@ -63,213 +63,55 @@ public class CanyonSurfaceBuilder extends SurfaceBuilder<SurfaceBuilderConfig> {
         super(codec);
     }
 
+    SurfaceContext context = null;
+
+
     @SuppressWarnings("ConstantConditions")
     @Override
     public void buildSurface(Random random, IChunk chunkIn, Biome biomeIn, int x, int z, int startHeight, double surfaceNoise, BlockState defaultBlock, BlockState defaultFluid, int seaLevel, long seed, SurfaceBuilderConfig config) {
         setupNoise(seed);
+        // A null SurfaceContext indicates we're not the SURFACE stage of ChunkStatus so exit early.
         BlockPos.Mutable localPos = new BlockPos.Mutable().setPos(x & 15, 0, z & 15);
-        for (int i = 0; i <= 180; i++) {
-            chunkIn.setBlockState(localPos, defaultBlock, false);
-            localPos.move(Direction.UP);
+        boolean isFinalIteration = localPos.getX() == 15 && localPos.getZ() == 15;
+        boolean isFirstIteration = localPos.getX() == 0 && localPos.getZ() == 0;
+
+        if (isFirstIteration) {
+            context = SurfaceContext.peek();
+        }
+        if (context == null) {
+            return;
+        }
+
+        ChunkGenerator generator = context.getWorld().getChunkProvider().getChunkGenerator();
+
+        double density = 0;
+        for (int neighborXSearch = -searchRange; neighborXSearch < searchRange; neighborXSearch++) {
+            for (int neighborZSearch = -searchRange; neighborZSearch < searchRange; neighborZSearch++) {
+                int neighborX = neighborXSearch;
+                int neighborZ = neighborZSearch;
+                Biome neighborBiome = generator.getBiomeProvider().getNoiseBiome(((x + neighborX) << 2) + 2, startHeight, ((x + neighborZ) << 2) + 2);
+                if (neighborBiome != biomeIn) {
+                    int neighborHeight = 63; //generator.getHeight(x + neighborX, z + neighborZ, Heightmap.Type.OCEAN_FLOOR_WG);
+                    density += neighborHeight * 0.1F;
+                }
+            }
+        }
+
+
+        double height = 120 + density;
+        for (int i = 0; i <= height; i++) {
+//            if (height <= 180) {
+                chunkIn.setBlockState(localPos, defaultBlock, false);
+                localPos.move(Direction.UP);
+//            }
         }
 
         localPos.setY(chunkIn.getTopBlockY(Heightmap.Type.OCEAN_FLOOR_WG, localPos.getX(), localPos.getZ()));
         chunkIn.setBlockState(localPos, BYGBlocks.OVERGROWN_STONE.getDefaultState(), false);
 
-        startHeight = 180;
-//        int localX = x & 15;
-//        int localZ = z & 15;
-//        BlockPos.Mutable localPos = new BlockPos.Mutable(localX, 100, localZ);
-//
-//        try {
-//            int centerMaxY = readHeightmapData(x, z, 100, 205);
-//
-//
-////            double totalDensity = 0;
-////            for (int xSearch = -searchRange; xSearch < searchRange; xSearch++) {
-////                for (int zSearch = -searchRange; zSearch < searchRange; zSearch++) {
-////                    int offsetMaxY = readHeightmapData(x + xSearch, z + zSearch, 100, 205);
-////
-////                    totalDensity += offsetMaxY * 0.01;
-////                }
-////            }
-//
-//
-//
-//
-//            for (int i = 100; i < centerMaxY; i++) {
-//                chunkIn.setBlockState(localPos, Blocks.STONE.getDefaultState(), false);
-//                localPos.move(Direction.UP);
-//            }
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//        int localX = x & 15;
-//        int localZ = z & 15;
-//
-//
-//        BlockPos.Mutable localPos = new BlockPos.Mutable(localX, startHeight, localZ);
-//
-//
-//        double curvingMultiplier = 0.4;
-//
-//        double noiseVal = noise.GetNoise(x, z) * 10;
-//        double reversedNoiseVal = noiseVal - Math.abs(noiseVal + noiseVal);
-//        float noise2Val = noise2.GetNoise(x, z);
-//        double curviness = Math.abs(noise2Val * curvingMultiplier);
-//        double minThreshold = 5.65 + curviness;
-//        double maxThreshold = 5.8 + curviness;
-//
-//        double offset3 = 0.65;
-//        double offset2 = 0.2;
-//        double offset = 0.45;
-//
-//        boolean check = noiseVal > minThreshold && noiseVal < maxThreshold;
-//        float simplexNoise = octaveSimplexNoise.GetNoise(x, z) * 7;
-//
-//        if (check) {
-//
-//            float totalCellDensity = 0;
-//
-//            for (int xSearch = -searchRange; xSearch < searchRange; xSearch++) {
-//                for (int zSearch = -searchRange; zSearch < searchRange; zSearch++) {
-//                    float rawOffsetNoiseGridNoise = noiseGrid.GetNoise(x + xSearch + 25, z + zSearch + 25);
-//                    float rawOffsetCellWeight = cellWeight(rawOffsetNoiseGridNoise).getFirst();
-//                    float cellWeight = CELL_WEIGHTS[xSearch + 2 + (zSearch + 2) * 5] / (rawOffsetNoiseGridNoise + 7.5F);
-//                    totalCellDensity += cellWeight * rawOffsetCellWeight * 0.2;
-//                }
-//            }
-//
-//
-//            double minThresholdNoiseValDif = noiseVal - minThreshold;
-//            double maxThresholdNoiseValDif = noiseVal - maxThreshold;
-//
-//            float lowestY = (float) (100 - (noiseVal * 25) + seaLevel + 40) - totalCellDensity + ((simplexNoise * 1.2F) * 0.5F);
-//            if (checkForFlip(minThresholdNoiseValDif, maxThresholdNoiseValDif)) {
-//                lowestY = (float) (100 + (reversedNoiseVal * 25) - seaLevel + 54) - totalCellDensity + ((simplexNoise * 1.2F) * 0.5F);
-//                lowestY = Math.abs(lowestY);
-//            }
-//
-//            for (int y = startHeight; y >= lowestY; y--) {
-//                if (y < seaLevel) {
-//                    chunkIn.setBlockState(localPos, Blocks.WATER.getDefaultState(), false);
-//                    chunkIn.getFluidsToBeTicked().scheduleTick(localPos, Fluids.WATER, 0);
-//                } else {
-//                    chunkIn.setBlockState(localPos, Blocks.AIR.getDefaultState(), false);
-//                }
-//                localPos.move(Direction.DOWN);
-//            }
-//        } else if (noiseVal > minThreshold - offset2 && noiseVal < maxThreshold + offset2) {
-//            float totalCellDensity = 0;
-//
-//            for (int xSearch = -searchRange; xSearch < searchRange; xSearch++) {
-//                for (int zSearch = -searchRange; zSearch < searchRange; zSearch++) {
-//                    float rawOffsetNoiseGridNoise = noiseGrid.GetNoise(x + xSearch, z + zSearch);
-//                    float rawOffsetCellWeight = cellWeight(rawOffsetNoiseGridNoise).getFirst();
-//                    float cellWeight = CELL_WEIGHTS[xSearch + 2 + (zSearch + 2) * 5] / (rawOffsetNoiseGridNoise + 7.5F);
-//                    totalCellDensity += cellWeight * rawOffsetCellWeight * 0.2;
-//                }
-//            }
-//
-//            float lowestY = (float) (100 - (noiseVal * 25) + 188) - totalCellDensity + (simplexNoise * 0.5F);
-//
-//            double minThresholdNoiseValDif = noiseVal - minThreshold;
-//            double maxThresholdNoiseValDif = noiseVal - maxThreshold;
-//
-//            if (checkForFlip(minThresholdNoiseValDif, maxThresholdNoiseValDif)) {
-//                lowestY = (float) (100 + (reversedNoiseVal * 24) - 96) - totalCellDensity + (simplexNoise * 0.5F);
-//                lowestY = Math.abs(lowestY);
-//            }
-//
-//            for (int y = startHeight; y >= lowestY; y--) {
-//                if (chunkIn.getBlockState(localPos).getMaterial() == Material.ROCK) {
-//                    if (y < seaLevel) {
-//                        chunkIn.setBlockState(localPos, Blocks.WATER.getDefaultState(), false);
-//                        chunkIn.getFluidsToBeTicked().scheduleTick(localPos, Fluids.WATER, 0);
-//                    } else {
-//                        chunkIn.setBlockState(localPos, Blocks.AIR.getDefaultState(), false);
-//                    }
-//                }
-//
-//                localPos.move(Direction.DOWN);
-//            }
-//            SurfaceBuilder.DEFAULT.buildSurface(random, chunkIn, biomeIn, x, z, startHeight, surfaceNoise, defaultBlock, defaultFluid, seaLevel, seed, new SurfaceBuilderConfig(Blocks.RED_SAND.getDefaultState(), Blocks.RED_SAND.getDefaultState(), Blocks.RED_SAND.getDefaultState()));
-//        } else if (noiseVal > minThreshold - offset && noiseVal < maxThreshold + offset) {
-//            float totalCellDensity = 0;
-//
-//            for (int xSearch = -searchRange; xSearch < searchRange; xSearch++) {
-//                for (int zSearch = -searchRange; zSearch < searchRange; zSearch++) {
-//                    float rawOffsetNoiseGridNoise = noiseGrid.GetNoise(x + xSearch, z + zSearch);
-//                    float rawOffsetCellWeight = cellWeight(rawOffsetNoiseGridNoise).getFirst();
-//                    float cellWeight = CELL_WEIGHTS[xSearch + 2 + (zSearch + 2) * 5] / (rawOffsetNoiseGridNoise + 7.5F);
-//                    totalCellDensity += cellWeight * rawOffsetCellWeight * 0.5;
-//                }
-//            }
-//
-//            float lowestY = (float) (100 - (noiseVal * 25) + 205) - totalCellDensity + simplexNoise;
-//
-//            double minThresholdNoiseValDif = noiseVal - minThreshold;
-//            double maxThresholdNoiseValDif = noiseVal - maxThreshold;
-//
-//            if (checkForFlip(minThresholdNoiseValDif, maxThresholdNoiseValDif)) {
-//                lowestY = (float) (100 + (reversedNoiseVal * 24) - 110) - totalCellDensity + simplexNoise;
-//                lowestY = Math.abs(lowestY);
-//            }
-//
-//            for (int y = startHeight + 5; y >= lowestY; y--) {
-//                if (chunkIn.getBlockState(localPos).getMaterial() == Material.ROCK) {
-//                    chunkIn.setBlockState(localPos, Blocks.AIR.getDefaultState(), false);
-//                }
-//                localPos.move(Direction.DOWN);
-//            }
-//        } else if (noiseVal > minThreshold - offset3 && noiseVal < maxThreshold + offset3) {
-//            float totalCellDensity = 0;
-//
-//            for (int xSearch = -searchRange; xSearch < searchRange; xSearch++) {
-//                for (int zSearch = -searchRange; zSearch < searchRange; zSearch++) {
-//                    float rawOffsetNoiseGridNoise = noiseGrid.GetNoise(x + xSearch, z + zSearch);
-//                    float rawOffsetCellWeight = cellWeight(rawOffsetNoiseGridNoise).getFirst();
-//                    float cellWeight = CELL_WEIGHTS[xSearch + 2 + (zSearch + 2) * 5] / (rawOffsetNoiseGridNoise + 7.5F);
-//                    totalCellDensity += cellWeight * rawOffsetCellWeight * 0.5;
-//                }
-//            }
-//
-//            float lowestY = (float) (100 - (noiseVal * 25) + 215) - totalCellDensity + simplexNoise;
-//
-//            double minThresholdNoiseValDif = noiseVal - minThreshold;
-//            double maxThresholdNoiseValDif = noiseVal - maxThreshold;
-//
-//            if (checkForFlip(minThresholdNoiseValDif, maxThresholdNoiseValDif)) {
-//                lowestY = (float) (100 + (reversedNoiseVal * 24) - 120) - totalCellDensity + simplexNoise;
-//                lowestY = Math.abs(lowestY);
-//            }
-//
-//            for (int y = startHeight + 5; y >= lowestY; y--) {
-//                if (chunkIn.getBlockState(localPos).getMaterial() == Material.ROCK) {
-//                    chunkIn.setBlockState(localPos, Blocks.AIR.getDefaultState(), false);
-//                }
-//                localPos.move(Direction.DOWN);
-//            }
-//        }
-//
-//        localPos.setPos(localX, startHeight, localZ);
-//        for (int height = startHeight; height > 0; height--) {
-//            if (chunkIn.getBlockState(localPos).getMaterial() == Material.ROCK) {
-//
-//                int blockY = localPos.getY();
-//                if (blockY % 6 == 0) {
-//                    chunkIn.setBlockState(localPos, Blocks.TERRACOTTA.getDefaultState(), false);
-//                } else if (blockY % 8 == 0) {
-//                    chunkIn.setBlockState(localPos, Blocks.RED_TERRACOTTA.getDefaultState(), false);
-//                } else {
-//                    chunkIn.setBlockState(localPos, BYGBlocks.RED_ROCK.getDefaultState(), false);
-//                }
-//            }
-//            localPos.move(Direction.DOWN);
-//        }
+        if (isFinalIteration) {
+            SurfaceContext.pop();
+        }
     }
 
     public static void setupNoise(long serverSeed) {
@@ -303,23 +145,9 @@ public class CanyonSurfaceBuilder extends SurfaceBuilder<SurfaceBuilderConfig> {
         }
     }
 
-
-    public static Pair<Float, BlockState> cellWeight(double cellNoiseVal) {
-        if (cellNoiseVal > 0.8) { //Eroded region
-            return new Pair<>(0.4F, Blocks.ANDESITE.getDefaultState());
-        } else if (cellNoiseVal > 0.2) {
-            return new Pair<>(0.75F, Blocks.CRYING_OBSIDIAN.getDefaultState());
-        } else if (cellNoiseVal > -0.4) {
-            return new Pair<>(0.65F, Blocks.JACK_O_LANTERN.getDefaultState());
-        } else {
-            return new Pair<>(0.55F, Blocks.DIORITE.getDefaultState());
-        }
-    }
-
     public static boolean checkForFlip(double min, double max) {
         return Math.abs(min) > Math.abs(max);
     }
-
 
     static int minColor = Integer.MAX_VALUE;
     static int maxColor = Integer.MIN_VALUE;
