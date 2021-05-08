3c9d39d9a505f86acb49876bf752beb72f2ba566
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-05-07 21:03:06 -0700

    -Fix tall flowers. Closes #455

--- src/main/java/corgiaoc/byg/common/properties/blocks/BYGTallFlowerBlock.java
+++ src/main/java/corgiaoc/byg/common/properties/blocks/BYGTallFlowerBlock.java
@@ -3,6 +3,8 @@ package corgiaoc.byg.common.properties.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.TallFlowerBlock;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.state.properties.DoubleBlockHalf;
 import net.minecraft.tags.ITag;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
@@ -17,13 +19,14 @@ public class BYGTallFlowerBlock extends TallFlowerBlock {
     }
 
     @Override
-    protected boolean mayPlaceOn(BlockState state, IBlockReader worldIn, BlockPos pos) {
+    protected boolean mayPlaceOn(BlockState state, IBlockReader world, BlockPos pos) {
         return state.is(validGround);
     }
 
     @Override
     public boolean canSurvive(BlockState state, IWorldReader worldIn, BlockPos pos) {
         BlockPos blockpos = pos.below();
-        return this.mayPlaceOn(worldIn.getBlockState(blockpos), worldIn, blockpos);
+        BlockState blockStateDown = worldIn.getBlockState(blockpos);
+        return state.getValue(BlockStateProperties.DOUBLE_BLOCK_HALF) == DoubleBlockHalf.UPPER ? blockStateDown.is(this) : this.mayPlaceOn(blockStateDown, worldIn, blockpos);
     }
 }
