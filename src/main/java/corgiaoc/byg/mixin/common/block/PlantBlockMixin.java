650dac77092cb588c1120716e880d4b2e43d3fd6
Corgi_Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-08-24 23:13:04 -0700

    Fix mixin target

--- src/main/java/corgiaoc/byg/mixin/common/block/PlantBlockMixin.java
+++ src/main/java/corgiaoc/byg/mixin/common/block/PlantBlockMixin.java
@@ -2,6 +2,7 @@ package corgiaoc.byg.mixin.common.block;
 
 import corgiaoc.byg.util.MLBlockTags;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.BushBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -12,8 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BushBlock.class)
 public class PlantBlockMixin {
-    @Inject(at = @At("RETURN"), method = "canSurvive", cancellable = true)
-    private void isBYGDirt(BlockState blockState, LevelReader levelReader, BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
+    @Inject(at = @At("RETURN"), method = "mayPlaceOn", cancellable = true)
+    private void isBYGDirt(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CallbackInfoReturnable<Boolean> cir) {
         if (blockState.is(MLBlockTags.DIRT)) {
             cir.setReturnValue(true);
         }
