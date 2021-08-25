eba3b193c2ef84348b621a3667456c66074490cf
Corgi_Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-08-24 21:00:23 -0700

    Fix containertypes

--- src/main/java/corgiaoc/byg/core/world/BYGContainerTypes.java
+++ src/main/java/corgiaoc/byg/core/world/BYGContainerTypes.java
@@ -13,12 +13,11 @@ import net.minecraft.world.inventory.MenuType;
 import java.util.ArrayList;
 import java.util.List;
 
-@Environment(EnvType.CLIENT)
 public class BYGContainerTypes {
 
     public static final List<MenuType<?>> CONTAINER_TYPES = new ArrayList<>();
 
-    public static final MenuType<HypogealImperiumContainer> HYPOGEAL_CONTAINER = register("bygcampfire", HypogealImperiumContainer::new);
+    public static final MenuType<HypogealImperiumContainer> HYPOGEAL_CONTAINER = register("hypogeal", HypogealImperiumContainer::new);
 
 
     private static <T extends AbstractContainerMenu> MenuType<T> register(String key, MenuType.MenuSupplier<T> builder) {
