ccb43dba754d5247d6b5081638d384fde8dea55c
Corgi_Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-09-22 11:45:06 -0700

    Use more efficient check order

--- src/main/java/corgiaoc/byg/mixin/common/block/MixinAbstractBlockStateBookshelf.java
+++ src/main/java/corgiaoc/byg/mixin/common/block/MixinAbstractBlockStateBookshelf.java
@@ -16,15 +16,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(BlockBehaviour.BlockStateBase.class)
 public abstract class MixinAbstractBlockStateBookshelf {
 
-    @Shadow public abstract boolean is(Tag<Block> tag);
+    @Shadow
+    public abstract boolean is(Tag<Block> tag);
 
     @SuppressWarnings("unchecked")
     @Inject(at = @At("HEAD"), method = "is(Lnet/minecraft/world/level/block/Block;)Z", cancellable = true)
     private void isBookshelf(Block block, CallbackInfoReturnable<Boolean> info) {
         // We need to make sure we're bounded first before using
-        if (MLBlockTags.BOOKSHELVES instanceof StaticTagHelper.Wrapper<Block> bookShelves){
-            if (((StaticTagHelperWrapperAccess<Block>) bookShelves).getTag() != null) {
-                if (block == Blocks.BOOKSHELF) {
+        if (block == Blocks.BOOKSHELF) {
+            if (MLBlockTags.BOOKSHELVES instanceof StaticTagHelper.Wrapper<Block> bookShelves) {
+                if (((StaticTagHelperWrapperAccess<Block>) bookShelves).getTag() != null) {
                     info.setReturnValue(this.is(bookShelves));
                 }
             }
