e1442a8af16aefb61adb8da7915796c9dd1e4a45
Bartosz Skrzypczak <Barteks2x@gmail.com>
2021-09-27 02:20:25 +0200

    Avoid generating floating islands in Column feature (#572)

--- src/main/java/corgiaoc/byg/common/world/feature/Column.java
+++ src/main/java/corgiaoc/byg/common/world/feature/Column.java
@@ -42,17 +42,30 @@ public class Column extends Feature<NoisySphereConfig> {
 
         for (int stackIDX = 0; stackIDX < 2; stackIDX++) {
             for (int x = -xRadius; x <= xRadius; x++) {
+                float xFract = x / (float) xRadius;
                 for (int z = -zRadius; z <= zRadius; z++) {
+                    float zFract = z / (float) zRadius;
                     for (int y = -yRadius; y <= yRadius + 15; y++) {
+                        float yFract = y / (float) yRadius;
+
                         mutable2.set(mutable).move(x, y, z);
                         //Credits to Hex_26 for this equation!
-                        double equationResult = Math.pow(x, 2) / Math.pow(xRadius, 2) + Math.pow(y, 2) / Math.pow(yRadius, 2) + Math.pow(z, 2) / Math.pow(zRadius, 2);
-                        float noise = use2D ? fastNoise.GetNoise(mutable2.getX(), mutable2.getZ()) : fastNoise.GetNoise(mutable2.getX(), mutable2.getY() / 1.5F, mutable2.getZ());
-                        if (noise < 0) {
-                            int i = 0;
+                        float distanceSquaredFromCenter = xFract * xFract + yFract * yFract + zFract * zFract;
+                        float yDistSquared = yFract * yFract;
+                        float noise = use2D ? fastNoise.GetNoise(mutable2.getX(), mutable2.getZ()) : fastNoise.GetNoise(mutable2.getX(), mutable2.getY(), mutable2.getZ());
+                        float threshold = 1 + 0.7f * noise;
+
+                        // check whether the center of this column at this Y would be empty
+                        float factor = yDistSquared / threshold;
+                        if (factor >= 1) {
+                            // make sure it's not empty
+                            distanceSquaredFromCenter /= factor;
+                            // add some extra noise back to make it not just a thin one block column
+                            // the magnitude of the noise can be configurable
+                            // this should probably be using its own noise for best effect
+                            distanceSquaredFromCenter -= Math.copySign(noise * 0.2, distanceSquaredFromCenter);
                         }
-                        double threshold = 1 + 0.7 * noise;
-                        if (equationResult >= threshold) {
+                        if (distanceSquaredFromCenter >= threshold) {
                             continue;
                         }
 
