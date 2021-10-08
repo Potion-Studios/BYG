5e45f6afb8567b609c91c613bda97533bfe1f980
Corgi_Taco <66983020+CorgiTaco@users.noreply.github.com>
2021-10-08 12:19:10 -0700

    Make sure BYG isn't wrongfully blamed for null items passed into the ColorManager

--- src/main/java/corgiaoc/byg/client/textures/ColorManager.java
+++ src/main/java/corgiaoc/byg/client/textures/ColorManager.java
@@ -1,5 +1,6 @@
 package corgiaoc.byg.client.textures;
 
+import corgiaoc.byg.BYG;
 import corgiaoc.byg.core.BYGBlocks;
 import corgiaoc.byg.core.BYGItems;
 import net.minecraft.block.BlockState;
@@ -11,6 +12,8 @@ import net.minecraft.world.FoliageColors;
 import net.minecraft.world.GrassColors;
 import net.minecraft.world.biome.BiomeColors;
 
+import java.io.IOException;
+
 public class ColorManager {
 
     public static synchronized void onBlockColorsInit(BlockColors blockColors) {
@@ -20,24 +23,28 @@ public class ColorManager {
     }
 
     public static synchronized void onItemColorsInit(BlockColors blockColors, ItemColors itemColors) {
-        // Use the Block's colour handler for an ItemBlock
-        IItemColor itemBlockColourHandler = (stack, tintIndex) -> {
-            BlockState state = ((BlockItem) stack.getItem()).getBlock().defaultBlockState();
-            return blockColors.getColor(state, null, null, tintIndex);
-        };
+        try {
+            // Use the Block's colour handler for an ItemBlock
+            IItemColor itemBlockColourHandler = (stack, tintIndex) -> {
+                BlockState state = ((BlockItem) stack.getItem()).getBlock().defaultBlockState();
+                return blockColors.getColor(state, null, null, tintIndex);
+            };
 
-        BYGItems.init(); // Make sure we've initialized.
+            BYGItems.init(); // Make sure we've initialized.
 
-        if (itemBlockColourHandler != null) {
-            itemColors.register(itemBlockColourHandler,
-                    BYGItems.MEADOW_GRASSBLOCK, BYGItems.OVERGROWN_NETHERRACK, BYGItems.MAHOGANY_LEAVES,
-                    BYGItems.POISON_IVY, BYGItems.OVERGROWN_STONE, BYGItems.SHORT_GRASS,
-                    BYGItems.TINY_LILYPADS, BYGItems.NETHER_BRISTLE, BYGItems.OVERGROWN_DACITE,
-                    BYGItems.GLOWCELIUM, BYGItems.WILTED_GRASS, BYGItems.WEED_GRASS, BYGItems.LEAF_PILE,
-                    BYGItems.ORCHARD_LEAVES, BYGItems.FLOWERING_ORCHARD_LEAVES, BYGItems.RIPE_ORCHARD_LEAVES,
-                    BYGItems.RAINBOW_EUCALYPTUS_LEAVES, BYGItems.WILLOW_LEAVES, BYGItems.WATER_SILK,
-                    BYGItems.MAPLE_LEAVES, BYGItems.JOSHUA_LEAVES, BYGItems.RIPE_JOSHUA_LEAVES, BYGBlocks.MOSSY_NETHERRACK);
+            if (itemBlockColourHandler != null) {
+                itemColors.register(itemBlockColourHandler,
+                        BYGItems.MEADOW_GRASSBLOCK, BYGItems.OVERGROWN_NETHERRACK, BYGItems.MAHOGANY_LEAVES,
+                        BYGItems.POISON_IVY, BYGItems.OVERGROWN_STONE, BYGItems.SHORT_GRASS,
+                        BYGItems.TINY_LILYPADS, BYGItems.NETHER_BRISTLE, BYGItems.OVERGROWN_DACITE,
+                        BYGItems.GLOWCELIUM, BYGItems.WILTED_GRASS, BYGItems.WEED_GRASS, BYGItems.LEAF_PILE,
+                        BYGItems.ORCHARD_LEAVES, BYGItems.FLOWERING_ORCHARD_LEAVES, BYGItems.RIPE_ORCHARD_LEAVES,
+                        BYGItems.RAINBOW_EUCALYPTUS_LEAVES, BYGItems.WILLOW_LEAVES, BYGItems.WATER_SILK,
+                        BYGItems.MAPLE_LEAVES, BYGItems.JOSHUA_LEAVES, BYGItems.RIPE_JOSHUA_LEAVES, BYGBlocks.MOSSY_NETHERRACK);
+            }
+        } catch (Exception e) {
+            BYG.LOGGER.error("This is not BYG's fault... although the stack trace may make it seem that way, one or several of your mods is managing to pass null items into the color manager and it's causing this crash. Find the mod and report it to them IMMEDIATELY! If this crash occurs with BYG on its own, then yes report it to BYG!");
+            e.printStackTrace();
         }
-
     }
 }
