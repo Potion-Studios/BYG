ba670324bffd7bb77ba62e567fb8a0d94e7d5ff9
YaBoiChips <63213388+YaBoiChips@users.noreply.github.com>
2022-02-19 15:59:40 -0700

    - QuartzSpikeFeature no longer manually places feature and uses noise.

--- Common/src/main/java/potionstudios/byg/common/world/feature/nether/quartzdesert/QuartzSpikeFeature.java
+++ Common/src/main/java/potionstudios/byg/common/world/feature/nether/quartzdesert/QuartzSpikeFeature.java
@@ -2,21 +2,27 @@ package potionstudios.byg.common.world.feature.nether.quartzdesert;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.predicate.BlockStatePredicate;
 import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.level.material.Material;
 import potionstudios.byg.common.block.BYGBlocks;
-import potionstudios.byg.common.world.feature.FeatureUtil;
 import potionstudios.byg.common.world.feature.config.QuartzSpikeConfig;
+import potionstudios.byg.common.world.math.noise.fastnoise.FastNoise;
 
 import java.util.Random;
 
 public class QuartzSpikeFeature extends Feature<QuartzSpikeConfig> {
     private static final BlockStatePredicate IS_QUARTZ_SAND = BlockStatePredicate.forBlock(BYGBlocks.QUARTZITE_SAND);
+
+    FastNoise noise = null;
 
 
     public QuartzSpikeFeature(Codec<QuartzSpikeConfig> codec) {
@@ -28,88 +34,51 @@ public class QuartzSpikeFeature extends Feature<QuartzSpikeConfig> {
         return place(featurePlaceContext.level(), featurePlaceContext.chunkGenerator(), featurePlaceContext.random(), featurePlaceContext.origin(), featurePlaceContext.config());
     }
 
-    public boolean place(WorldGenLevel world, ChunkGenerator generator, Random random, BlockPos pos, QuartzSpikeConfig config) {
+    public boolean place(WorldGenLevel world, ChunkGenerator generator, Random rand, BlockPos pos, QuartzSpikeConfig config) {
+        setSeed(world.getSeed());
 
-        if (!IS_QUARTZ_SAND.test(world.getBlockState(pos.below()))){
-            return false;
-        }
-
-        if (!world.getBlockState(pos.above(4)).canOcclude()){
-            return false;
-        }
-
-        if (!FeatureUtil.isAir(world, pos.above(5))) {
-            return false;
-        }
-
-        if (world.getBlockState(pos.below()) != config.getBlockProvider().getState(random, pos) || !world.getBlockState(pos.below()).canOcclude())
+        if (world.getBlockState(pos.below()).getMaterial() == Material.AIR || world.getBlockState(pos.below()) != BYGBlocks.QUARTZITE_SAND.defaultBlockState() || world.getBlockState(pos.below()).getMaterial() == Material.LAVA || world.getHeight(Heightmap.Types.OCEAN_FLOOR_WG, pos.getX(), pos.getZ()) < 4)
             return false;
 
-        int type = random.nextInt(2);
 
+        BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos();
 
-            if (type == 1) {
-                for (int y1 = 1; y1 <= 2; ++y1) {
-                    world.setBlock(pos.offset(1, y1, -1), getQuartzType(random), 3);
-                    world.setBlock(pos.offset(0, y1, -1), getQuartzType(random), 3);
-                    world.setBlock(pos.offset(-1, y1, 0), getQuartzType(random), 3);
-                    world.setBlock(pos.offset(0, y1, 2), getQuartzType(random), 3);
-                }
-                for (int y1 = 1; y1 <= 3; ++y1) {
-                    world.setBlock(pos.offset(2, y1, 0), getQuartzType(random), 3);
-                    world.setBlock(pos.offset(-1, y1, 1), getQuartzType(random), 3);
-                    world.setBlock(pos.offset(1, y1, 1), getQuartzType(random), 3);
-                    world.setBlock(pos.offset(2, y1, 1), getQuartzType(random), 3);
-                }
-                for (int y1 = 1; y1 <= 5; ++y1) {
-                    world.setBlock(pos.offset(0, y1, 0), getQuartzType(random), 3);
-                }
-                for (int y1 = 1; y1 <= 6; ++y1) {
-                    world.setBlock(pos.offset(1, y1, 0), getQuartzType(random), 3);
-                }
-                for (int y1 = 1; y1 <= 7; ++y1) {
-                    world.setBlock(pos.offset(0, y1, 1), getQuartzType(random), 3);
-                }
-                for (int y1 = 1; y1 <= 9; ++y1) {
-                    world.setBlock(pos.offset(1, y1, 1), getQuartzType(random), 3);
-                }
-            } else {
-                for (int y1 = 1; y1 <= 2; ++y1) {
-                    world.setBlock(pos.offset(-1, y1, 1), getQuartzType(random), 3);
-                    world.setBlock(pos.offset(0, y1, -1), getQuartzType(random), 3);
-                    world.setBlock(pos.offset(-1, y1, 0), getQuartzType(random), 3);
-                    world.setBlock(pos.offset(2, y1, 0), getQuartzType(random), 3);
-                }
-                for (int y1 = 1; y1 <= 3; ++y1) {
-                    world.setBlock(pos.offset(1, y1, 2), getQuartzType(random), 3);
-                    world.setBlock(pos.offset(-1, y1, 0), getQuartzType(random), 3);
-                }
-                for (int y1 = 1; y1 <= 4; ++y1) {
-                    world.setBlock(pos.offset(0, y1, 2), getQuartzType(random), 3);
-                    world.setBlock(pos.offset(1, y1, 2), getQuartzType(random), 3);
-                    world.setBlock(pos.offset(1, y1, 0), getQuartzType(random), 3);
-                }
-                for (int y1 = 1; y1 <= 6; ++y1) {
-                    world.setBlock(pos.offset(0, y1, 0), getQuartzType(random), 3);
-                }
-                for (int y1 = 1; y1 <= 7; ++y1) {
-                    world.setBlock(pos.offset(1, y1, 1), getQuartzType(random), 3);
-                }
-                for (int y1 = 1; y1 <= 9; ++y1) {
-                    world.setBlock(pos.offset(0, y1, 1), getQuartzType(random), 3);
+        double baseRadius = 1;
+        int height = 10;
+        int startHeight = height - 3;
+
+        for (double y = -height; y <= -1; y++) {
+            for (double x = -height; x <= height; x++) {
+                for (double z = -height; z <= height; z++) {
+                    mutable.set(pos).move((int) x, (int) y + startHeight, (int) z);
+                    double noise = this.noise.GetNoise((float) mutable.getX(), (float) mutable.getZ()) * 12;
+                    double scaledNoise = (noise / 11) * (-(y * baseRadius) / ((x * x) + (z * z)));
+                    double threshold = 0.5;
+
+                    if (y == -height) {
+                        if (scaledNoise >= threshold)
+                            if (world.getBlockState(mutable.relative(Direction.DOWN)).getMaterial() == Material.AIR)
+                                return false;
+                    }
+
+                    if (scaledNoise >= threshold) {
+                        if (world.getBlockState(mutable).getMaterial() == Material.AIR) {
+                            BlockState blockState = config.getBlockProvider().getState(rand, mutable);
+                            world.setBlock(mutable, blockState, 2);
+                        }
+                    }
                 }
             }
-
+        }
         return true;
-
     }
 
-    public static BlockState getQuartzType(Random rand){
-        int i = rand.nextInt(5);
-        if (i <= 3){
-            return Blocks.SMOOTH_QUARTZ.defaultBlockState();
-        } else{
-            return Blocks.QUARTZ_BLOCK.defaultBlockState();
+    public void setSeed(long seed) {
+        if (noise == null) {
+            noise = new FastNoise((int) seed);
+            noise.SetNoiseType(FastNoise.NoiseType.PerlinFractal);
+            noise.SetFractalType(FastNoise.FractalType.FBM);
+            noise.SetFrequency(0.2F);
         }
     }
 }
