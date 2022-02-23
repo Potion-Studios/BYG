6265efd2c3de201a57f65c87cdadc9df244dbc81
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2022-02-23 00:35:23 -0800

    Disable therium deposits (for now)

--- Common/src/main/java/potionstudios/byg/common/world/biome/BYGDefaultBiomeFeatures.java
+++ Common/src/main/java/potionstudios/byg/common/world/biome/BYGDefaultBiomeFeatures.java
@@ -472,7 +472,7 @@ public class BYGDefaultBiomeFeatures {
     }
 
     public static void addTheriumDeposit(BiomeGenerationSettings.Builder gen) {
-        gen.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, BYGPlacedFeatures.THERIUM_DEPOSIT);
+//        gen.addFeature(GenerationStep.Decoration.RAW_GENERATION, BYGPlacedFeatures.THERIUM_DEPOSIT);
     }
 
     public static void addEtherFoliage(BiomeGenerationSettings.Builder gen) {
