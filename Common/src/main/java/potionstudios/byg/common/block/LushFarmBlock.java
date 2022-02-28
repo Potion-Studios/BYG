2d2671fdfb35659465d4b479b75565716ae98633
AOCAWOL <biomesyougo@gmail.com>
2022-02-28 10:55:58 -0700

    -Updated Lush Farmland to utilize Therium Glint particle

--- Common/src/main/java/potionstudios/byg/common/block/LushFarmBlock.java
+++ Common/src/main/java/potionstudios/byg/common/block/LushFarmBlock.java
@@ -7,6 +7,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.FarmBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import potionstudios.byg.client.textures.renders.BYGParticleTypes;
 import potionstudios.byg.util.BYGUtil;
 
 import java.util.Random;
@@ -37,7 +38,7 @@ public class LushFarmBlock extends FarmBlock {
 
         if (random.nextInt(10) == 0 && BYGUtil.isNearby(level, blockPos, THERIUM_LANTERN_SEARCH_RANGE, IS_THERIUM_LANTERN)) {
 //            level.addParticle(ParticleTypes.WARPED_SPORE, blockPos.getX() + 0.5D, blockPos.getY() + 2.0D, blockPos.getZ() + 0.5D, (double) random.nextDouble() - 0.5D, random.nextDouble() - 1.0F, random.nextDouble() - 0.5D);
-            level.addParticle(ParticleTypes.HAPPY_VILLAGER, (double)blockPos.getX() + random.nextDouble(), (double)blockPos.getY() + 1.1D, (double)blockPos.getZ() + random.nextDouble(), 0.0D, 0.0D, 0.0D);
+            level.addParticle(BYGParticleTypes.THERIUM_GLINT, (double)blockPos.getX() + random.nextDouble(), (double)blockPos.getY() + 1.1D, (double)blockPos.getZ() + random.nextDouble(), 0.0D, 0.0D, 0.0D);
         }
     }
 
--- Common/src/main/java/potionstudios/byg/common/particles/TheriumGlint.java
+++ Common/src/main/java/potionstudios/byg/common/particles/TheriumGlint.java
@@ -10,6 +10,16 @@ public class TheriumGlint extends TextureSheetParticle {
 
     protected TheriumGlint(ClientLevel world, double x, double y, double z, double xSpeed, double ySpeed, double zSpeed) {
         super(world, x, y, z, xSpeed, ySpeed, zSpeed);
+        float $$7 = this.random.nextFloat() * 0.1F + 0.2F;
+        this.rCol = $$7;
+        this.gCol = $$7;
+        this.bCol = $$7;
+        this.setSize(0.02F, 0.02F);
+        this.quadSize *= this.random.nextFloat() * 0.6F + 0.5F;
+        this.xd *= 0.019999999552965164D;
+        this.yd *= 0.019999999552965164D;
+        this.zd *= 0.019999999552965164D;
+        this.lifetime = (int)(20.0D / (Math.random() * 0.8D + 0.2D));
     }
 
     @Override
@@ -18,15 +28,23 @@ public class TheriumGlint extends TextureSheetParticle {
     }
 
     @Override
+    public void move(double $$0, double $$1, double $$2) {
+        this.setBoundingBox(this.getBoundingBox().move($$0, $$1, $$2));
+        this.setLocationFromBoundingbox();
+    }
+
+    @Override
     public void tick() {
         this.xo = this.x;
         this.yo = this.y;
         this.zo = this.z;
-        if (this.age-- <= 0) {
+        if (this.lifetime-- <= 0) {
             this.remove();
-        }else{
+        } else {
             this.move(this.xd, this.yd, this.zd);
-            this.xd *= 1.0;
+            this.xd *= 0.99D;
+            this.yd *= 0.99D;
+            this.zd *= 0.99D;
         }
     }
 
@@ -45,5 +63,6 @@ public class TheriumGlint extends TextureSheetParticle {
             glint.setSpriteFromAge(this.sprite);
             return glint;
         }
+
     }
 }
