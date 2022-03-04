7103362778d69a20ec1e4c71f9f03112a1d653db
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2022-03-03 21:00:57 -0800

    Arches generate much cooler now :)

--- Common/src/main/java/potionstudios/byg/common/world/feature/gen/overworld/ArchFeature.java
+++ Common/src/main/java/potionstudios/byg/common/world/feature/gen/overworld/ArchFeature.java
@@ -2,7 +2,6 @@ package potionstudios.byg.common.world.feature.gen.overworld;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
 import net.minecraft.util.Mth;
 import net.minecraft.util.random.SimpleWeightedRandomList;
 import net.minecraft.util.valueproviders.UniformInt;
@@ -14,12 +13,13 @@ import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.stateproviders.WeightedStateProvider;
-import potionstudios.byg.common.block.BYGBlocks;
 import potionstudios.byg.common.world.feature.BYGFeatures;
 import potionstudios.byg.common.world.feature.config.NoisySphereConfig;
 import potionstudios.byg.common.world.feature.config.SimpleBlockProviderConfig;
 import potionstudios.byg.common.world.math.noise.fastnoise.FastNoise;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
@@ -40,35 +40,52 @@ public class ArchFeature extends Feature<SimpleBlockProviderConfig> {
 
     public boolean place(WorldGenLevel world, ChunkGenerator chunkGenerator, Random random, BlockPos pos, SimpleBlockProviderConfig config) {
         setSeed(world.getSeed());
-        BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos().set(pos);
-        int curveLength = 10;
-        boolean zAxis = random.nextInt(2) == 0;
+        double angle = random.nextDouble(Math.PI);
 
-        BlockPos.MutableBlockPos mutable2 = new BlockPos.MutableBlockPos();
-        float curviness = Mth.lerp(random.nextFloat(), 0.05F, 0.4F);
-        for (int z = -curveLength; z <= curveLength; z++) {
-            mutable2.set(mutable.getX() + (!zAxis ? z : 0), 0, mutable.getZ() + (zAxis ? z : 0));
-            int height = getArchHeight(z, curviness);
-            mutable2.move(Direction.UP, height + pos.getY() + 15);
+        int distance = 12;
+        int archHeight = 20;
+        pos = pos.offset(0, archHeight, 0);
 
-            int worldHeight = world.getHeight(Heightmap.Types.WORLD_SURFACE_WG, mutable2.getX(), mutable2.getZ());
-            if(worldHeight - 5 < mutable2.getY()) {
-                FeaturePlaceContext<NoisySphereConfig> featurePlaceContext = new FeaturePlaceContext<>(Optional.empty(), world, chunkGenerator, random, mutable2, new NoisySphereConfig.Builder().withBlockProvider(new WeightedStateProvider(new SimpleWeightedRandomList.Builder<BlockState>().add(Blocks.TERRACOTTA.defaultBlockState(), 5).add(BYGBlocks.RED_ROCK.defaultBlockState(), 5))).withRadiusSettings(new NoisySphereConfig.RadiusSettings(UniformInt.of(4, 7), UniformInt.of(7, 10), 0, UniformInt.of(4, 7))).build());
-                BYGFeatures.NOISE_SPHERE.place(featurePlaceContext);
-                // Make sure we find a floor and connect it.
-                if (z == -curveLength || z == curveLength) {
-                    while (mutable2.getY() > worldHeight - 5) {
-                        BYGFeatures.NOISE_SPHERE.place(featurePlaceContext);
-                        mutable2.move(Direction.DOWN, 3);
-                    }
-                }
-            }
+        double xOffset = Math.sin(angle + Math.PI) * distance;
+        double zOffset = Math.cos(angle + Math.PI) * distance;
+
+        NoisySphereConfig build = new NoisySphereConfig.Builder()
+            .withRadiusSettings(
+                new NoisySphereConfig.RadiusSettings(UniformInt.of(5, 10), UniformInt.of(5, 10), 0, UniformInt.of(5, 10))
+            ).withBlockProvider(
+                new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder()
+                    .add(Blocks.GRANITE.defaultBlockState(), 4)
+                    .add(Blocks.POLISHED_GRANITE.defaultBlockState(), 1)
+                )
+            ).withNoiseFrequency(0.02F)
+            .withTopBlockProvider(
+                new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder()
+                    .add(Blocks.GRANITE.defaultBlockState(), 4)
+                    .add(Blocks.POLISHED_GRANITE.defaultBlockState(), 1)
+                )
+            )
+            .build();
+
+        BlockPos start = pos.offset(-xOffset, 0, -zOffset);
+        start = new BlockPos(start.getX(), world.getHeight(Heightmap.Types.OCEAN_FLOOR_WG, start.getX(), start.getZ()), start.getZ());
+        BlockPos end = pos.offset(xOffset, 0, zOffset);
+        end = new BlockPos(end.getX(), world.getHeight(Heightmap.Types.OCEAN_FLOOR_WG, end.getX(), end.getZ()), end.getZ());
+
+
+        List<BlockPos> spherePositions = new ArrayList<>();
+
+        int points = 10;
+        for (int pointCount = points; pointCount >= 1; pointCount--) {
+            double lerpStrength = (double) pointCount / points;
+            spherePositions.add(new BlockPos(Mth.lerp(lerpStrength, start.getX(), pos.getX()), Mth.lerp(lerpStrength, start.getY(), pos.getY()), Mth.lerp(lerpStrength, start.getZ(), pos.getZ())));
+            spherePositions.add(new BlockPos(Mth.lerp(lerpStrength, pos.getX(), end.getX()), Mth.lerp(lerpStrength, pos.getY(), end.getY()), Mth.lerp(lerpStrength, pos.getZ(), end.getZ())));
         }
-        return true;
-    }
 
-    public static int getArchHeight(int z, float curviness) {
-        return (int) ((-z * z) * curviness);
+        for (BlockPos spherePosition : spherePositions) {
+            BYGFeatures.BOULDER.place(new FeaturePlaceContext<>(Optional.empty(), world, chunkGenerator, random, spherePosition, build));
+        }
+
+        return true;
     }
 
 
