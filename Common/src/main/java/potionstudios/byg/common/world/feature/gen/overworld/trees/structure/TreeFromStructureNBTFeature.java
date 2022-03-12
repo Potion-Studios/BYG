4139b459a9723ee20d942daec301417bafbe082b
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2022-03-12 13:54:55 -0800

    Continue structure tree implementation

--- Common/src/main/java/potionstudios/byg/common/world/feature/gen/overworld/trees/structure/TreeFromStructureNBTFeature.java
+++ Common/src/main/java/potionstudios/byg/common/world/feature/gen/overworld/trees/structure/TreeFromStructureNBTFeature.java
@@ -7,6 +7,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LeavesBlock;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.feature.Feature;
@@ -24,6 +25,7 @@ import potionstudios.byg.mixin.access.StructureTemplateAccess;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 public class TreeFromStructureNBTFeature extends Feature<NoneFeatureConfiguration> {
     public TreeFromStructureNBTFeature(Codec<NoneFeatureConfiguration> $$0) {
@@ -54,9 +56,18 @@ public class TreeFromStructureNBTFeature extends Feature<NoneFeatureConfiguratio
         List<StructureTemplate.Palette> basePalettes = ((StructureTemplateAccess) baseTemplate).byg_getPalettes();
         List<StructureTemplate.Palette> canopyPalettes = ((StructureTemplateAccess) canopyTemplate).byg_getPalettes();
         BlockPos origin = featurePlaceContext.origin();
-        StructurePlaceSettings placeSettings = new StructurePlaceSettings();
+//        level.setBlock(origin, Blocks.DIAMOND_BLOCK.defaultBlockState(), 2);
+        Random random = featurePlaceContext.random();
+        StructurePlaceSettings placeSettings = new StructurePlaceSettings().setRotation(Rotation.getRandom(random));
         StructureTemplate.Palette randomBasePalette = placeSettings.getRandomPalette(basePalettes, origin);
         StructureTemplate.Palette randomCanopyPalette = placeSettings.getRandomPalette(canopyPalettes, origin);
+
+        List<StructureTemplate.StructureBlockInfo> center = randomBasePalette.blocks(Blocks.WHITE_WOOL);
+        if (center.size() > 1) {
+            throw new IllegalArgumentException("There cannot be more than one central position.");
+        }
+        BlockPos centerOffset = center.get(0).pos;
+        centerOffset = new BlockPos(-centerOffset.getX(), 0, -centerOffset.getZ());
 
         List<StructureTemplate.StructureBlockInfo> leaves = randomCanopyPalette.blocks(BYGBlocks.MANGROVE_LEAVES);
         List<StructureTemplate.StructureBlockInfo> canopyLogs = randomCanopyPalette.blocks(BYGBlocks.MANGROVE_LOG);
@@ -67,7 +78,7 @@ public class TreeFromStructureNBTFeature extends Feature<NoneFeatureConfiguratio
         int trunkLength = 15;
 
         for (StructureTemplate.StructureBlockInfo logBuilder : logBuilders) {
-            BlockPos pos = logBuilder.pos.offset(origin);
+            BlockPos pos = getModifiedPos(placeSettings, logBuilder, centerOffset, origin);
             int maxTrunkBuildingDepth = 5;
             int yDifference = pos.getY() - level.getHeight(Heightmap.Types.OCEAN_FLOOR_WG, pos.getX(), pos.getZ());
             if (yDifference > maxTrunkBuildingDepth) {
@@ -76,7 +87,7 @@ public class TreeFromStructureNBTFeature extends Feature<NoneFeatureConfiguratio
         }
 
         for (StructureTemplate.StructureBlockInfo logBuilder : logBuilders) {
-            BlockPos pos = logBuilder.pos.offset(origin);
+            BlockPos pos = getModifiedPos(placeSettings, logBuilder, centerOffset, origin);
             BlockPos.MutableBlockPos mutableBlockPos = new BlockPos.MutableBlockPos().set(pos);
             while (!level.getBlockState(mutableBlockPos).canOcclude()) {
                 level.setBlock(mutableBlockPos, stateProvider.getState(level.getRandom(), mutableBlockPos), 2);
@@ -85,51 +96,63 @@ public class TreeFromStructureNBTFeature extends Feature<NoneFeatureConfiguratio
         }
 
         for (StructureTemplate.StructureBlockInfo trunk : logs) {
-            BlockPos pos = trunk.pos.offset(origin);
-            level.setBlock(pos, stateProvider.getState(featurePlaceContext.random(), pos), 2);
+            BlockPos pos = getModifiedPos(placeSettings, trunk, centerOffset, origin);
+            level.setBlock(pos, stateProvider.getState(random, pos), 2);
         }
 
         for (StructureTemplate.StructureBlockInfo trunk : trunkBuilder) {
-            BlockPos pos = trunk.pos.offset(origin);
+            BlockPos pos = getModifiedPos(placeSettings, trunk, centerOffset, origin);
             BlockPos.MutableBlockPos mutable = new BlockPos.MutableBlockPos().set(pos);
 
             for (int i = 0; i < trunkLength; i++) {
-                level.setBlock(mutable, stateProvider.getState(featurePlaceContext.random(), mutable), 2);
+                level.setBlock(mutable, stateProvider.getState(random, mutable), 2);
                 mutable.move(Direction.UP);
             }
         }
-        BlockPos canopyPos = origin.offset(0, trunkLength, 0);
+        {
+            List<StructureTemplate.StructureBlockInfo> canopyCenter = randomCanopyPalette.blocks(Blocks.WHITE_WOOL);
+            if (center.size() > 1) {
+                throw new IllegalArgumentException("There cannot be more than one central position.");
+            }
+            BlockPos canopyCenterOffset = canopyCenter.get(0).pos;
+            canopyCenterOffset = new BlockPos(-canopyCenterOffset.getX(), trunkLength + canopyCenterOffset.getY() + 1, -canopyCenterOffset.getZ());
 
-        for (StructureTemplate.StructureBlockInfo canopyLog : canopyLogs) {
-            BlockPos pos = canopyLog.pos.offset(canopyPos);
-            level.setBlock(pos, stateProvider.getState(featurePlaceContext.random(), pos), 2);
-        }
 
-        List<Runnable> leavesPostApply = new ArrayList<>(leaves.size());
-        for (StructureTemplate.StructureBlockInfo leaf : leaves) {
-            BlockPos pos = leaf.pos.offset(canopyPos);
+            for (StructureTemplate.StructureBlockInfo canopyLog : canopyLogs) {
+                BlockPos pos = getModifiedPos(placeSettings, canopyLog, canopyCenterOffset, origin);
+                level.setBlock(pos, stateProvider.getState(random, pos), 2);
+            }
 
-            BlockState state = leavesProvider.getState(featurePlaceContext.random(), pos);
-            if (level.getBlockState(pos).isAir()) {
-                level.setBlock(pos, state, 2);
-                Runnable postProcess = () -> {
-                    BlockState blockState = LeavesBlockAccess.byg_invokeUpdateDistance(state, level, pos);
-                    if (blockState.getValue(LeavesBlock.DISTANCE) < LeavesBlock.DECAY_DISTANCE) {
-                        level.setBlock(pos, blockState, 2);
-                        BlockPos below = pos.below();
-                        if (level.getBlockState(below).isAir()) {
-                            // Bottom positions here
+            List<Runnable> leavesPostApply = new ArrayList<>(leaves.size());
+            for (StructureTemplate.StructureBlockInfo leaf : leaves) {
+                BlockPos pos = getModifiedPos(placeSettings, leaf, canopyCenterOffset, origin);
+
+                BlockState state = leavesProvider.getState(random, pos);
+                if (!level.getBlockState(pos).canOcclude()) {
+                    level.setBlock(pos, state, 2);
+                    Runnable postProcess = () -> {
+                        BlockState blockState = LeavesBlockAccess.byg_invokeUpdateDistance(state, level, pos);
+                        if (blockState.getValue(LeavesBlock.DISTANCE) < LeavesBlock.DECAY_DISTANCE) {
+                            level.setBlock(pos, blockState, 2);
+                            BlockPos below = pos.below();
+                            if (level.getBlockState(below).isAir()) {
+                                // Bottom positions here
+                            }
+
+                        } else {
+                            level.removeBlock(pos, false);
                         }
-
-                    } else {
-                        level.removeBlock(pos, false);
-                    }
-                };
-                leavesPostApply.add(postProcess);
+                    };
+                    leavesPostApply.add(postProcess);
+                }
             }
             leavesPostApply.forEach(Runnable::run);
         }
         return false;
+    }
+
+    private static BlockPos getModifiedPos(StructurePlaceSettings settings, StructureTemplate.StructureBlockInfo placing, BlockPos partCenter, BlockPos featureOrigin) {
+        return StructureTemplate.calculateRelativePosition(settings, placing.pos).offset(featureOrigin).offset(StructureTemplate.calculateRelativePosition(settings, partCenter));
     }
 
     public static IllegalArgumentException noTreePartPresent(ResourceLocation location) {
