ab164da8ae551508d586c01b2a40591a5494f26e
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2022-03-13 13:02:58 -0700

    BYG is officially out of Alpha :D! Disable Experimental Warning, Bump version to `1.4 Release Candidate 1`.

--- Common/src/main/java/potionstudios/byg/BYG.java
+++ Common/src/main/java/potionstudios/byg/BYG.java
@@ -40,7 +40,7 @@ public class BYG {
     public static final String MOD_ID = "byg";
     public static final Logger LOGGER = LogManager.getLogger();
     public static final boolean BIOMES = true;
-    public static final boolean WARN_EXPERIMENTAL = true;
+    public static final boolean WARN_EXPERIMENTAL = false;
     public static final int EXPERIMENTAL_WARNING_VERSION = 5;
     public static final boolean DEV_ENVIRONMENT = Boolean.parseBoolean(System.getProperty("bygDev", "false"));
     public static boolean ENABLE_OVERWORLD_TREES = true;
