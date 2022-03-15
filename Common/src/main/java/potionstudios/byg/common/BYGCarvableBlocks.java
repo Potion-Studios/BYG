f898d0e5fabd3e4ea2db8d93a931c4bf70567f29
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2022-03-15 15:03:59 -0700

    Add BYG's Nether & End Carver blocks. Fixes #672

--- Common/src/main/java/potionstudios/byg/common/BYGCarvableBlocks.java
+++ Common/src/main/java/potionstudios/byg/common/BYGCarvableBlocks.java
@@ -6,10 +6,26 @@ import potionstudios.byg.common.block.BYGBlocks;
 
 import java.util.Set;
 
+import static potionstudios.byg.common.block.BYGBlocks.*;
+
 public class BYGCarvableBlocks {
 
     @SuppressWarnings("deprecation")
     public static Set<Block> addCarverBlocks() {
-        return ImmutableSet.of(BYGBlocks.OVERGROWN_DACITE, BYGBlocks.PODZOL_DACITE, BYGBlocks.SOAPSTONE, BYGBlocks.ROCKY_STONE, BYGBlocks.MOSSY_STONE, BYGBlocks.OVERGROWN_STONE, BYGBlocks.SCORIA_STONE, BYGBlocks.LUSH_DIRT, BYGBlocks.PEAT);
+        return ImmutableSet.of(
+            OVERGROWN_DACITE, PODZOL_DACITE, SOAPSTONE,
+            ROCKY_STONE, MOSSY_STONE, OVERGROWN_STONE,
+            SCORIA_STONE, LUSH_DIRT, PEAT,
+            OVERGROWN_NETHERRACK, PERVADED_NETHERRACK, BLUE_NETHERRACK,
+            MYCELIUM_NETHERRACK, EMBUR_NYLIUM, WAILING_NYLIUM,
+            SYTHIAN_NYLIUM, OVERGROWN_CRIMSON_BLACKSTONE, SUBZERO_ASH_BLOCK,
+            WARPED_SOUL_SAND, WARPED_SOUL_SOIL, BRIMSTONE,
+            QUARTZITE_SAND, BLUE_NETHER_QUARTZ_ORE, BLUE_NETHER_GOLD_ORE,
+            EMERALDITE_ORE, PENDORITE_ORE, AMETRINE_ORE,
+            BUDDING_AMETRINE_ORE, CRYPTIC_REDSTONE_ORE, CRYPTIC_STONE,
+            MAGMATIC_STONE, CRYPTIC_MAGMA_BLOCK, ETHER_STONE,
+            VERMILION_SCULK, IMPARIUS_PHYLIUM, IVIS_PHYLIUM,
+            SHULKREN_PHYLIUM, ETHER_PHYLIUM, NIGHTSHADE_PHYLIUM
+        );
     }
 }
