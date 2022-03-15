3684710e729c90b9d2bbe29af25d952cb77adfe1
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2022-03-15 15:21:56 -0700

    Warped Desert ceilings spawn warped soul soil + sand now. Fixes #671

--- Common/src/main/java/potionstudios/byg/common/world/surfacerules/BYGSurfaceRules.java
+++ Common/src/main/java/potionstudios/byg/common/world/surfacerules/BYGSurfaceRules.java
@@ -192,10 +192,15 @@ public class BYGSurfaceRules {
         SurfaceRules.sequence(
             SurfaceRules.ifTrue(byg_invokeSurfaceNoiseAbove(-0.95D), SurfaceRules.sequence(
                 SurfaceRules.ifTrue(SurfaceRules.UNDER_FLOOR, SurfaceRules.state(BYGBlocks.WARPED_SOUL_SAND.defaultBlockState())),
-                SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, SurfaceRules.state(BYGBlocks.WARPED_SOUL_SAND.defaultBlockState()))
+                SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, SurfaceRules.state(BYGBlocks.WARPED_SOUL_SAND.defaultBlockState())),
+                SurfaceRules.ifTrue(SurfaceRules.ON_CEILING, SurfaceRules.state(BYGBlocks.WARPED_SOUL_SAND.defaultBlockState())),
+                SurfaceRules.ifTrue(SurfaceRules.UNDER_CEILING, SurfaceRules.state(BYGBlocks.WARPED_SOUL_SAND.defaultBlockState()))
             )),
             SurfaceRules.ifTrue(SurfaceRules.UNDER_FLOOR, SurfaceRules.state(BYGBlocks.WARPED_SOUL_SOIL.defaultBlockState())),
-            SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, SurfaceRules.state(BYGBlocks.WARPED_SOUL_SOIL.defaultBlockState()))
+            SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, SurfaceRules.state(BYGBlocks.WARPED_SOUL_SOIL.defaultBlockState())),
+            SurfaceRules.ifTrue(SurfaceRules.ON_CEILING, SurfaceRules.state(BYGBlocks.WARPED_SOUL_SOIL.defaultBlockState())),
+            SurfaceRules.ifTrue(SurfaceRules.UNDER_CEILING, SurfaceRules.state(BYGBlocks.WARPED_SOUL_SOIL.defaultBlockState()))
+
         )
     );
     public static final SurfaceRules.RuleSource END_SURFACE_RULES = SurfaceRules.sequence(BULBIS_PHYCELIUM, NIGHTSHADE_PHYLIUM, IMPARIUS_PHYLIUM, IVIS_PHYLIUM, SHULKREN_PHYLIUM, ETHEREAL_ISLANDS, CRYPTIC_FILL);
