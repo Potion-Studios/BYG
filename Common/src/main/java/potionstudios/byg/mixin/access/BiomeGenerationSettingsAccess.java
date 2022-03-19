e677b75c8d4ee4f52a21a00f35def47afbe5cd84
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2022-03-18 18:55:00 -0700

    Make features mutable

--- Common/src/main/java/potionstudios/byg/mixin/access/BiomeGenerationSettingsAccess.java
+++ Common/src/main/java/potionstudios/byg/mixin/access/BiomeGenerationSettingsAccess.java
@@ -4,6 +4,7 @@ import net.minecraft.core.HolderSet;
 import net.minecraft.world.level.biome.BiomeGenerationSettings;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
 @Mixin(BiomeGenerationSettings.class)
 public interface BiomeGenerationSettingsAccess {
 
+    @Mutable
     @Accessor("features")
     void byg_setFeatures(List<HolderSet<PlacedFeature>> features);
 }
