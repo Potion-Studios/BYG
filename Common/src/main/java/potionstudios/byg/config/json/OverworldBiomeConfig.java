dc5134f1a7e341998cfffdbc4a8ad899690c2c95
CorgiTaco <66983020+CorgiTaco@users.noreply.github.com>
2022-03-20 18:55:30 -0700

    Add a commented config system

--- Common/src/main/java/potionstudios/byg/config/json/OverworldBiomeConfig.java
+++ Common/src/main/java/potionstudios/byg/config/json/OverworldBiomeConfig.java
@@ -1,26 +1,61 @@
 package potionstudios.byg.config.json;
 
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
+import blue.endless.jankson.*;
+import blue.endless.jankson.api.SyntaxError;
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.JsonOps;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.Util;
 import potionstudios.byg.BYG;
 import potionstudios.byg.common.world.biome.overworld.BYGOverworldBiomeBuilders;
 import potionstudios.byg.util.BYGUtil;
+import potionstudios.byg.util.JanksonJsonOps;
 
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public record OverworldBiomeConfig(boolean generateOverworld,
                                    List<BYGOverworldBiomeBuilders.BiomeProviderData> values) {
     public static final OverworldBiomeConfig DEFAULT = new OverworldBiomeConfig(true, BYGOverworldBiomeBuilders.OVERWORLD_DEFAULTS);
+    private static final Jankson JANKSON = Jankson.builder().build();
 
     public static OverworldBiomeConfig INSTANCE = null;
+
+    public static final Map<String, String> COMMENTS = Util.make(new HashMap<>(), map -> {
+        map.put("overworld_enabled", "Global toggle to enable or disable BYG biomes.");
+
+
+        String biomeLayout = """
+            [ARID-ICY, DRY-ICY, NEUTRAL-ICY, WET-ICY, HUMID-ICY]
+            [ARID-COLD, DRY-COLD, NEUTRAL-COLD, WET-COLD, HUMID-COLD]
+            [ARID-NEUTRAL, DRY-NEUTRAL, NEUTRAL-NEUTRAL, WET-NEUTRAL, HUMID-NEUTRAL]
+            [ARID-WARM, DRY-WARM, NEUTRAL-WARM, WET-WARM, HUMID-WARM]
+            [ARID-HOT, DRY-HOT, NEUTRAL-HOT, WET-HOT, HUMID-HOT]
+            """;
+
+        String oceanLayout = """
+            [SHALLOW-ICY, SHALLOW-COLD, SHALLOW-NEUTRAL, SHALLOW-WARM, SHALLOW-HOT]
+            [DEEP-ICY, DEEP-COLD, DEEP-NEUTRAL, DEEP-WARM, DEEP-HOT]
+            """;
+
+        String requiresValidKeys = """
+            All keys passed in must be valid in the biome registry!
+            "minecraft:the_void" is invalid as it represents a value of "NULL(nothing)" internally.""";
+
+        map.put("providers.peak_biomes", "Appearing in mountainous terrain & BELOW weirdness 0, here is the \"peak_biomes\" layout:\n" + biomeLayout + requiresValidKeys);
+        map.put("providers.peak_biome_variants", "Appearing in mountainous terrain & ABOVE weirdness 0, here is the \"peak_biome_variants\" layout:\n" + biomeLayout + requiresValidKeys);
+        map.put("providers.ocean_biomes", "Appearing in terrain below sea level, here is the \"ocean_biomes\" layout:\n" + oceanLayout + requiresValidKeys);
+        map.put("providers.beach_biomes", "Appearing in terrain bordering oceans, here is the \"beach_biomes\" layout:\n" + biomeLayout + requiresValidKeys);
+        map.put("providers.weight", """
+            The weight of this provider/region against all other providers.
+            Higher numbers do NOT increase the provider's size.
+            Weight "0" disables this provider and all it's entries."""
+        );
+    });
 
     public static final Codec<OverworldBiomeConfig> CODEC = RecordCodecBuilder.create(builder -> {
         return builder.group(
@@ -43,24 +78,96 @@ public record OverworldBiomeConfig(boolean generateOverworld,
     }
 
     private static OverworldBiomeConfig readConfig() {
-        final Path path = BYG.CONFIG_PATH.resolve("overworld-biomes.json");
+
+        final Path path = BYG.CONFIG_PATH.resolve("overworld-biomes.json5");
+        OverworldBiomeConfig from = readAndDeleteOldOverworldConfig(BYG.CONFIG_PATH.resolve("overworld-biomes.json"), DEFAULT);
 
         if (!path.toFile().exists()) {
-            JsonElement jsonElement = CODEC.encodeStart(JsonOps.INSTANCE, DEFAULT).result().get();
-
-            try {
-                Files.createDirectories(path.getParent());
-                Files.write(path, new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create().toJson(jsonElement).getBytes());
-            } catch (IOException e) {
-                BYG.LOGGER.error(e.toString());
-            }
+            createConfig(path, from);
         }
-        BYG.LOGGER.info(String.format("\"%s\" was read.", path.toString()));
 
         try {
-            return CODEC.decode(JsonOps.INSTANCE, new JsonParser().parse(new FileReader(path.toFile()))).result().orElseThrow(() -> BYGUtil.configFileFailureException(path)).getFirst();
-        } catch (IOException e) {
+            OverworldBiomeConfig overworldBiomeConfig = readOverworldBiomeConfig(path, JANKSON);
+            BYG.LOGGER.info(String.format("\"%s\" was read.", path.toString()));
+            return overworldBiomeConfig;
+        } catch (IOException | SyntaxError e) {
             throw new IllegalStateException(e);
         }
+    }
+
+    private static OverworldBiomeConfig readAndDeleteOldOverworldConfig(Path oldPath, OverworldBiomeConfig from) {
+        if (oldPath.toFile().exists()) {
+            try {
+                from = readOverworldBiomeConfig(oldPath, JANKSON);
+                Files.delete(oldPath);
+            } catch (IOException | SyntaxError e) {
+                e.printStackTrace();
+            }
+        }
+        return from;
+    }
+
+    private static OverworldBiomeConfig readOverworldBiomeConfig(Path path, Jankson jankson) throws IOException, SyntaxError {
+        return CODEC.decode(JanksonJsonOps.INSTANCE, jankson.load(path.toFile())).resultOrPartial(BYG.LOGGER::error)
+            .orElseThrow(() -> BYGUtil.configFileFailureException(path)).getFirst();
+    }
+
+    private static void createConfig(Path path, OverworldBiomeConfig config) {
+        JsonElement jsonElement = CODEC.encodeStart(JanksonJsonOps.INSTANCE, config).result().orElseThrow();
+
+        if (jsonElement instanceof JsonObject jsonObject) {
+            jsonElement = addCommentsAndAlphabeticallySortRecursively(COMMENTS, jsonObject, "", true);
+        }
+        try {
+            Files.createDirectories(path.getParent());
+            String str = jsonElement.toJson(new JsonGrammar.Builder().withComments(true).bareSpecialNumerics(true).printCommas(true).build());
+            Files.write(path, str.getBytes());
+        } catch (IOException e) {
+            BYG.LOGGER.error(e.toString());
+        }
+    }
+
+    private static JsonObject addCommentsAndAlphabeticallySortRecursively(Map<String, String> comments, JsonObject object, String parentKey, boolean alphabeticallySorted) {
+        for (Map.Entry<String, JsonElement> entry : object.entrySet()) {
+            String objectKey = entry.getKey();
+            String commentsKey = parentKey + objectKey;
+
+            String comment = null;
+            if (comments.containsKey(commentsKey)) {
+                String commentToAdd = comments.get(commentsKey);
+                object.setComment(objectKey, commentToAdd);
+                comment = commentToAdd;
+            }
+
+            JsonElement value = entry.getValue();
+            if (value instanceof JsonArray array) {
+                JsonArray sortedJsonElements = new JsonArray();
+                for (JsonElement element : array) {
+                    if (element instanceof JsonObject nestedObject) {
+                        sortedJsonElements.add(addCommentsAndAlphabeticallySortRecursively(comments, nestedObject, entry.getKey() + ".", alphabeticallySorted));
+                    }
+                }
+                if (!sortedJsonElements.isEmpty()) {
+                    object.put(objectKey, sortedJsonElements, comment);
+                }
+            }
+
+            if (value instanceof JsonObject nestedObject) {
+                object.put(objectKey, addCommentsAndAlphabeticallySortRecursively(comments, nestedObject, entry.getKey() + ".", alphabeticallySorted), comment);
+            }
+        }
+
+        if (alphabeticallySorted) {
+            JsonObject alphabeticallySortedJsonObject = new JsonObject();
+            TreeMap<String, JsonElement> map = new TreeMap<>(String::compareTo);
+            map.putAll(object);
+            alphabeticallySortedJsonObject.putAll(map);
+            alphabeticallySortedJsonObject.forEach((key, entry) -> {
+                alphabeticallySortedJsonObject.setComment(key, object.getComment(key));
+            });
+
+            return alphabeticallySortedJsonObject;
+        }
+        return object;
     }
 }
---
+++ Common/src/main/java/potionstudios/byg/util/JanksonJsonOps.java
@@ -0,0 +1,422 @@
+package potionstudios.byg.util;
+
+import blue.endless.jankson.*;
+import com.google.common.collect.Lists;
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.*;
+
+import javax.annotation.Nullable;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public record JanksonJsonOps(boolean compressed) implements DynamicOps<JsonElement> {
+    public static final JanksonJsonOps INSTANCE = new JanksonJsonOps(false);
+    public static final JanksonJsonOps COMPRESSED = new JanksonJsonOps(true);
+
+    @Override
+    public JsonElement empty() {
+        return JsonNull.INSTANCE;
+    }
+
+    @Override
+    public <U> U convertTo(final DynamicOps<U> outOps, final JsonElement input) {
+        if (input instanceof JsonObject) {
+            return convertMap(outOps, input);
+        }
+        if (input instanceof JsonArray) {
+            return convertList(outOps, input);
+        }
+        if (input instanceof JsonNull) {
+            return outOps.empty();
+        }
+        final JsonPrimitive primitive = (JsonPrimitive) input;
+        Object value = primitive.getValue();
+        if (value instanceof String) {
+            return outOps.createString(primitive.asString());
+        }
+        if (value instanceof Boolean) {
+            return outOps.createBoolean(primitive.asBoolean(false));
+        }
+
+
+        final BigDecimal bigDecimal = primitive.asBigDecimal(new BigDecimal(primitive.asString()));
+        try {
+            final long l = bigDecimal.longValueExact();
+            if ((byte) l == l) {
+                return outOps.createByte((byte) l);
+            }
+            if ((short) l == l) {
+                return outOps.createShort((short) l);
+            }
+            if ((int) l == l) {
+                return outOps.createInt((int) l);
+            }
+            return outOps.createLong(l);
+        } catch (final ArithmeticException e) {
+            final double d = bigDecimal.doubleValue();
+            if ((float) d == d) {
+                return outOps.createFloat((float) d);
+            }
+            return outOps.createDouble(d);
+        }
+    }
+
+    @Override
+    public DataResult<Number> getNumberValue(final JsonElement input) {
+        if (input instanceof JsonPrimitive jsonPrimitive) {
+            Object value = jsonPrimitive.getValue();
+            if (value instanceof Number number) {
+                return DataResult.success(number);
+            } else if (value instanceof Boolean bl) {
+                return DataResult.success(bl ? 1 : 0);
+            }
+            if (compressed && value instanceof String s) {
+                try {
+                    return DataResult.success(Integer.parseInt(s));
+                } catch (final NumberFormatException e) {
+                    return DataResult.error("Not a number: " + e + " " + input);
+                }
+            }
+        }
+        if (input instanceof JsonPrimitive primitive && primitive.getValue() instanceof Boolean) {
+            return DataResult.success((Boolean) primitive.getValue() ? 1 : 0);
+        }
+        return DataResult.error("Not a number: " + input);
+    }
+
+    @Override
+    public JsonElement createNumeric(final Number i) {
+        return new JsonPrimitive(i);
+    }
+
+    @Override
+    public DataResult<Boolean> getBooleanValue(final JsonElement input) {
+        if (input instanceof JsonPrimitive) {
+            Object value = ((JsonPrimitive) input).getValue();
+            if (value instanceof Boolean bl) {
+                return DataResult.success(bl);
+            } else if (value instanceof Number num) {
+                return DataResult.success(num.byteValue() != 0);
+            }
+        }
+        return DataResult.error("Not a boolean: " + input);
+    }
+
+    @Override
+    public JsonElement createBoolean(final boolean value) {
+        return new JsonPrimitive(value);
+    }
+
+    @Override
+    public DataResult<String> getStringValue(final JsonElement input) {
+        if (input instanceof JsonPrimitive primitive) {
+            if (primitive.getValue() instanceof String || primitive.getValue() instanceof Number && compressed) {
+                return DataResult.success(primitive.asString());
+            }
+        }
+        return DataResult.error("Not a string: " + input);
+    }
+
+    @Override
+    public JsonElement createString(final String value) {
+        return new JsonPrimitive(value);
+    }
+
+    @Override
+    public DataResult<JsonElement> mergeToList(final JsonElement list, final JsonElement value) {
+        if (!(list instanceof JsonArray) && list != empty()) {
+            return DataResult.error("mergeToList called with not a list: " + list, list);
+        }
+
+        final JsonArray result = new JsonArray();
+        if (list != empty()) {
+            result.addAll((Collection<? extends JsonElement>) list);
+        }
+        result.add(value);
+        return DataResult.success(result);
+    }
+
+    @Override
+    public DataResult<JsonElement> mergeToList(final JsonElement list, final List<JsonElement> values) {
+        if (!(list instanceof JsonArray) && list != empty()) {
+            return DataResult.error("mergeToList called with not a list: " + list, list);
+        }
+
+        final JsonArray result = new JsonArray();
+        if (list != empty()) {
+            result.addAll((Collection<? extends JsonElement>) list);
+        }
+        values.forEach(result::add);
+        return DataResult.success(result);
+    }
+
+    @Override
+    public DataResult<JsonElement> mergeToMap(final JsonElement map, final JsonElement key, final JsonElement value) {
+        if (!(map instanceof JsonObject) && map != empty()) {
+            return DataResult.error("mergeToMap called with not a map: " + map, map);
+        }
+        if (!(key instanceof JsonPrimitive primitive) || !(primitive.getValue() instanceof String) && !compressed) {
+            return DataResult.error("key is not a string: " + key, map);
+        }
+
+        final JsonObject output = new JsonObject();
+        if (map != empty()) {
+            ((JsonObject) map).entrySet().forEach(entry -> output.put(entry.getKey(), entry.getValue()));
+        }
+        output.put(((JsonPrimitive) key).asString(), value);
+
+        return DataResult.success(output);
+    }
+
+    @Override
+    public DataResult<JsonElement> mergeToMap(final JsonElement map, final MapLike<JsonElement> values) {
+        if (!(map instanceof JsonObject) && map != empty()) {
+            return DataResult.error("mergeToMap called with not a map: " + map, map);
+        }
+
+        final JsonObject output = new JsonObject();
+        if (map != empty()) {
+            ((JsonObject) map).entrySet().forEach(entry -> output.put(entry.getKey(), entry.getValue()));
+        }
+
+        final List<JsonElement> missed = Lists.newArrayList();
+
+        values.entries().forEach(entry -> {
+            final JsonElement key = entry.getFirst();
+            if (!(key instanceof JsonPrimitive primitive) || !(primitive.getValue() instanceof String) && !compressed) {
+                missed.add(key);
+                return;
+            }
+            output.put(((JsonPrimitive) key).asString(), entry.getSecond());
+        });
+
+        if (!missed.isEmpty()) {
+            return DataResult.error("some keys are not strings: " + missed, output);
+        }
+
+        return DataResult.success(output);
+    }
+
+    @Override
+    public DataResult<Stream<Pair<JsonElement, JsonElement>>> getMapValues(final JsonElement input) {
+        if (!(input instanceof JsonObject)) {
+            return DataResult.error("Not a JSON object: " + input);
+        }
+        return DataResult.success(((JsonObject) input).entrySet().stream().map(entry -> Pair.of(new JsonPrimitive(entry.getKey()), entry.getValue() instanceof JsonNull ? null : entry.getValue())));
+    }
+
+    @Override
+    public DataResult<Consumer<BiConsumer<JsonElement, JsonElement>>> getMapEntries(final JsonElement input) {
+        if (!(input instanceof JsonObject)) {
+            return DataResult.error("Not a JSON object: " + input);
+        }
+        return DataResult.success(c -> {
+            for (final Map.Entry<String, JsonElement> entry : ((JsonObject) input).entrySet()) {
+                c.accept(createString(entry.getKey()), entry.getValue() instanceof JsonNull ? null : entry.getValue());
+            }
+        });
+    }
+
+    @Override
+    public DataResult<MapLike<JsonElement>> getMap(final JsonElement input) {
+        if (!(input instanceof JsonObject)) {
+            return DataResult.error("Not a JSON object: " + input);
+        }
+        final JsonObject object = (JsonObject) input;
+        return DataResult.success(new MapLike<>() {
+            @Nullable
+            @Override
+            public JsonElement get(final JsonElement key) {
+                final JsonElement element = object.get(((JsonPrimitive) key).asString());
+                if (element instanceof JsonNull) {
+                    return null;
+                }
+                return element;
+            }
+
+            @Nullable
+            @Override
+            public JsonElement get(final String key) {
+                final JsonElement element = object.get(key);
+                if (element instanceof JsonNull) {
+                    return null;
+                }
+                return element;
+            }
+
+            @Override
+            public Stream<Pair<JsonElement, JsonElement>> entries() {
+                return object.entrySet().stream().map(e -> Pair.of(new JsonPrimitive(e.getKey()), e.getValue()));
+            }
+
+            @Override
+            public String toString() {
+                return "MapLike[" + object + "]";
+            }
+        });
+    }
+
+    @Override
+    public JsonElement createMap(final Stream<Pair<JsonElement, JsonElement>> map) {
+        final JsonObject result = new JsonObject();
+        map.forEach(p -> {
+            result.put(((JsonPrimitive) p.getFirst()).asString(), p.getSecond());
+        });
+        return result;
+    }
+
+    @Override
+    public DataResult<Stream<JsonElement>> getStream(final JsonElement input) {
+        if (input instanceof JsonArray) {
+            return DataResult.success(StreamSupport.stream(((JsonArray) input).spliterator(), false).map(e -> e instanceof JsonNull ? null : e));
+        }
+        return DataResult.error("Not a json array: " + input);
+    }
+
+    @Override
+    public DataResult<Consumer<Consumer<JsonElement>>> getList(final JsonElement input) {
+        if (input instanceof JsonArray) {
+            return DataResult.success(c -> {
+                for (final JsonElement element : ((JsonArray) input)) {
+                    c.accept(element instanceof JsonNull ? null : element);
+                }
+            });
+        }
+        return DataResult.error("Not a json array: " + input);
+    }
+
+    @Override
+    public JsonElement createList(final Stream<JsonElement> input) {
+        final JsonArray result = new JsonArray();
+        input.forEach(result::add);
+        return result;
+    }
+
+    @Override
+    public JsonElement remove(final JsonElement input, final String key) {
+        if (input instanceof JsonObject) {
+            final JsonObject result = new JsonObject();
+            ((JsonObject) input).entrySet().stream().filter(entry -> !Objects.equals(entry.getKey(), key)).forEach(entry -> result.put(entry.getKey(), entry.getValue()));
+            return result;
+        }
+        return input;
+    }
+
+    @Override
+    public String toString() {
+        return "Jankson JSON";
+    }
+
+    @Override
+    public ListBuilder<JsonElement> listBuilder() {
+        return new ArrayBuilder();
+    }
+
+    private static final class ArrayBuilder implements ListBuilder<JsonElement> {
+        private DataResult<JsonArray> builder = DataResult.success(new JsonArray(), Lifecycle.stable());
+
+        @Override
+        public DynamicOps<JsonElement> ops() {
+            return INSTANCE;
+        }
+
+        @Override
+        public ListBuilder<JsonElement> add(final JsonElement value) {
+            builder = builder.map(b -> {
+                b.add(value);
+                return b;
+            });
+            return this;
+        }
+
+        @Override
+        public ListBuilder<JsonElement> add(final DataResult<JsonElement> value) {
+            builder = builder.apply2stable((b, element) -> {
+                b.add(element);
+                return b;
+            }, value);
+            return this;
+        }
+
+        @Override
+        public ListBuilder<JsonElement> withErrorsFrom(final DataResult<?> result) {
+            builder = builder.flatMap(r -> result.map(v -> r));
+            return this;
+        }
+
+        @Override
+        public ListBuilder<JsonElement> mapError(final UnaryOperator<String> onError) {
+            builder = builder.mapError(onError);
+            return this;
+        }
+
+        @Override
+        public DataResult<JsonElement> build(final JsonElement prefix) {
+            final DataResult<JsonElement> result = builder.flatMap(b -> {
+                if (!(prefix instanceof JsonArray) && prefix != ops().empty()) {
+                    return DataResult.error("Cannot append a list to not a list: " + prefix, prefix);
+                }
+
+                final JsonArray array = new JsonArray();
+                if (prefix != ops().empty()) {
+                    array.addAll(((JsonArray) prefix));
+                }
+                array.addAll(b);
+                return DataResult.success(array, Lifecycle.stable());
+            });
+
+            builder = DataResult.success(new JsonArray(), Lifecycle.stable());
+            return result;
+        }
+    }
+
+    @Override
+    public boolean compressMaps() {
+        return compressed;
+    }
+
+    @Override
+    public RecordBuilder<JsonElement> mapBuilder() {
+        return new JsonRecordBuilder();
+    }
+
+    private class JsonRecordBuilder extends RecordBuilder.AbstractStringBuilder<JsonElement, JsonObject> {
+        protected JsonRecordBuilder() {
+            super(JanksonJsonOps.this);
+        }
+
+        @Override
+        protected JsonObject initBuilder() {
+            return new JsonObject();
+        }
+
+        @Override
+        protected JsonObject append(final String key, final JsonElement value, final JsonObject builder) {
+            builder.put(key, value);
+            return builder;
+        }
+
+        @Override
+        protected DataResult<JsonElement> build(final JsonObject builder, final JsonElement prefix) {
+            if (prefix == null || prefix instanceof JsonNull) {
+                return DataResult.success(builder);
+            }
+            if (prefix instanceof JsonObject) {
+                final JsonObject result = new JsonObject();
+                for (final Map.Entry<String, JsonElement> entry : ((JsonObject) prefix).entrySet()) {
+                    result.put(entry.getKey(), entry.getValue());
+                }
+                for (final Map.Entry<String, JsonElement> entry : builder.entrySet()) {
+                    result.put(entry.getKey(), entry.getValue());
+                }
+                return DataResult.success(result);
+            }
+            return DataResult.error("mergeToMap called with not a map: " + prefix, prefix);
+        }
+    }
+}
