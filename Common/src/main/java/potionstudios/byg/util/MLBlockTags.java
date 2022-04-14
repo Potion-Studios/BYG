1fb19e6f658d2ddb22e9026a597449b18d84367b
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2022-04-13 19:04:22 -0700

    Use plural tags internally

--- Common/src/main/java/potionstudios/byg/util/MLBlockTags.java
+++ Common/src/main/java/potionstudios/byg/util/MLBlockTags.java
@@ -8,11 +8,11 @@ import potionstudios.byg.BYG;
 
 public class MLBlockTags {
 
-    public static final TagKey<Block> NETHERRACK = createTag("netherrack");
+    public static final TagKey<Block> NETHERRACK = createTag("netherracks");
     public static final TagKey<Block> END_STONES = createTag("end_stones");
     public static final TagKey<Block> ORES = createTag("ores");
     public static final TagKey<Block> BOOKSHELVES = createTag("bookshelves");
-    public static final TagKey<Block> SANDSTONE = createTag("sandstone");
+    public static final TagKey<Block> SANDSTONE = createTag("sandstones");
 
     public static TagKey<Block> createTag(String path) {
         return TagKey.create(Registry.BLOCK_REGISTRY, new ResourceLocation(BYG.MOD_LOADER_TAG_TARGET, path));
