afc5a7900dd06a856b407f3b236ac35f231de387
Matyrobbrt <matyrobbrt@gmail.com>
2022-05-02 22:30:02 +0300

    Remove unused import

--- Forge/src/main/java/potionstudios/byg/BYGForge.java
+++ Forge/src/main/java/potionstudios/byg/BYGForge.java
@@ -70,7 +70,6 @@ import terrablender.api.SurfaceRuleManager;
 
 import java.nio.file.Path;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
