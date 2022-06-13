7aef1c099a333706b7b2c7766ff4ffe9dd5c3ca7
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2022-06-12 21:45:28 -0700

    Fix surface system badland extension for BYG biomes(Shatter Glacier & Sierra Badlands)

--- Common/src/main/java/potionstudios/byg/mixin/common/world/surface/MixinSurfaceSystem.java
+++ Common/src/main/java/potionstudios/byg/mixin/common/world/surface/MixinSurfaceSystem.java
@@ -39,7 +39,7 @@ public abstract class MixinSurfaceSystem implements SeedGetter {
     @Inject(method = "buildSurface", at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Holder;is(Lnet/minecraft/resources/ResourceKey;)Z", shift = At.Shift.BEFORE, ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD)
     private void addBYGErodedBadlandsExtension(RandomState $$0, BiomeManager $$1, Registry<Biome> $$2, boolean $$3, WorldGenerationContext $$4, ChunkAccess chunkAccess, NoiseChunk $$6, SurfaceRules.RuleSource $$7, CallbackInfo ci, BlockPos.MutableBlockPos $$8, ChunkPos $$9, int $$10, int $$11, BlockColumn blockColumn, SurfaceRules.Context $$13, SurfaceRules.SurfaceRule $$14, BlockPos.MutableBlockPos $$15, int $$16, int $$17, int $$18, int $$19, int $$20, Holder<Biome> biome) {
         if (biome.is(BYGBiomes.SHATTERED_GLACIER) || biome.is(BYGBiomes.SIERRA_BADLANDS)) {
-            this.erodedBadlandsExtension(blockColumn, $$17, $$18, $$19, chunkAccess);
+            this.erodedBadlandsExtension(blockColumn, $$18, $$19, $$20, chunkAccess);
         }
     }
 
