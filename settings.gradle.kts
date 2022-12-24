9c9a5c9de16ec8a17ea53842749ad3dd06520361
J.T.McQuigg <J.T.McQuigg12@gmail.com>
2022-12-24 18:13:35 -0500

    settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        maven {
+            name = "Sponge Snapshots"
+            url = uri("https://repo.spongepowered.org/repository/maven-public/")
+        }
+    }
+}
+
+rootProject.name = "BYG"
+include("Common", "Fabric", "Forge")
