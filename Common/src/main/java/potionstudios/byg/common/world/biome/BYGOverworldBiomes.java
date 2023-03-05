085c1e75e642b12805525eef66297c2d7f10b2b0
catter1 <63272345+catter1@users.noreply.github.com>
2023-03-05 05:32:38 +0100

    fix Feature Order Cycle in Maple Taiga (#1105)

--- Common/src/main/java/potionstudios/byg/common/world/biome/BYGOverworldBiomes.java
+++ Common/src/main/java/potionstudios/byg/common/world/biome/BYGOverworldBiomes.java
@@ -1043,8 +1043,8 @@ public class BYGOverworldBiomes {
         BiomeDefaultFeatures.addDefaultOres(generationSettings);
         BiomeDefaultFeatures.addDefaultSoftDisks(generationSettings);
         BiomeDefaultFeatures.addDefaultFlowers(generationSettings);
-        BiomeDefaultFeatures.addSavannaExtraGrass(generationSettings);
         BiomeDefaultFeatures.addTaigaGrass(generationSettings);
+        BiomeDefaultFeatures.addSavannaExtraGrass(generationSettings);
         BiomeDefaultFeatures.addDefaultExtraVegetation(generationSettings);
 
         BYGDefaultBiomeFeatures.addMapleTaigaTrees(generationSettings);
