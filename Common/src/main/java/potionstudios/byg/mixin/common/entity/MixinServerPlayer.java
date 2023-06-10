f5acf02e6e6464fe35b9ff8b760c2c307d340860
Corgi Taco <66983020+CorgiTaco@users.noreply.github.com>
2023-06-10 15:14:31 -0700

    Don't give biomepedia several times, only once. Fixes #1194

--- Common/src/main/java/potionstudios/byg/mixin/common/entity/MixinServerPlayer.java
+++ Common/src/main/java/potionstudios/byg/mixin/common/entity/MixinServerPlayer.java
@@ -5,6 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import potionstudios.byg.common.entity.player.extension.BiomepediaExtension;
 import potionstudios.byg.server.level.BYGPlayerTrackedData;
 import potionstudios.byg.util.BYGAdditionalData;
 
@@ -14,5 +15,10 @@ public abstract class MixinServerPlayer implements BYGAdditionalData, BYGPlayerT
     @Inject(method = "doTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/advancements/critereon/PlayerTrigger;trigger(Lnet/minecraft/server/level/ServerPlayer;)V"))
     private void updateBygServerPlayerTrackedData(CallbackInfo ci) {
         this.getPlayerTrackedData().tickPerSecond((ServerPlayer) (Object) this);
+    }
+
+    @Inject(method = "restoreFrom", at = @At("RETURN"))
+    private void keepBiomepediaFlags(ServerPlayer player, boolean keepEverything, CallbackInfo ci) {
+        ((BiomepediaExtension) this).setGotBiomepedia(((BiomepediaExtension) player).gotBiomepedia());
     }
 }
