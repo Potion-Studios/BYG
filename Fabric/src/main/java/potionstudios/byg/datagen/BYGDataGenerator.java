bb7818f5f3b48bd46c4a5f3e94f6c53ac695b641
AwesomeDude091 <computer.cubegeek4life@gmail.com>
2023-10-25 16:54:48 +0200

    The beginning of something awesome

---
+++ Fabric/src/main/java/potionstudios/byg/datagen/BYGDataGenerator.java
@@ -0,0 +1,13 @@
+package potionstudios.byg.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BYGDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator generator) {
+        FabricDataGenerator.Pack pack = generator.createPack();
+    }
+
+
+}
