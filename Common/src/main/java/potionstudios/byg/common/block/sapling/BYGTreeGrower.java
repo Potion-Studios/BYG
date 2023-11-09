79e5cc858a78e638984e11ddaab8524bf43823ab
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-11-09 11:27:02 -0500

    Fix BYGTreeGrower.java for 1.18.2

--- Common/src/main/java/potionstudios/byg/common/block/sapling/BYGTreeGrower.java
+++ Common/src/main/java/potionstudios/byg/common/block/sapling/BYGTreeGrower.java
@@ -3,22 +3,26 @@ package potionstudios.byg.common.block.sapling;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Random;
+
 public class BYGTreeGrower extends AbstractTreeGrower {
-    @Nullable
-    @Override
-    protected Holder<? extends ConfiguredFeature<?, ?>> getConfiguredFeature(RandomSource randomSource, boolean b) {
-        return null;
-    }
+
 
     @Override
-    public boolean growTree(ServerLevel $$0, ChunkGenerator $$1, BlockPos $$2, BlockState $$3, RandomSource $$4) {
+    public boolean growTree(@NotNull ServerLevel $$0, @NotNull ChunkGenerator $$1, @NotNull BlockPos $$2, @NotNull BlockState $$3, @NotNull Random $$4) {
         return false;
+    }
+
+    @Nullable
+    @Override
+    protected Holder<? extends ConfiguredFeature<?, ?>> getConfiguredFeature(@NotNull Random random, boolean b) {
+        return null;
     }
 }
