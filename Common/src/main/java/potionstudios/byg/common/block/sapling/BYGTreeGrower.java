c0c345346f2fb94f3f71b7bfb9872d9fe94c697c
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-11-09 20:09:48 -0500

    Fix BYGTreeGrower.java

--- Common/src/main/java/potionstudios/byg/common/block/sapling/BYGTreeGrower.java
+++ Common/src/main/java/potionstudios/byg/common/block/sapling/BYGTreeGrower.java
@@ -1,24 +1,23 @@
 package potionstudios.byg.common.block.sapling;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.NotNull;
 
 public class BYGTreeGrower extends AbstractTreeGrower {
-    @Nullable
     @Override
-    protected Holder<? extends ConfiguredFeature<?, ?>> getConfiguredFeature(RandomSource randomSource, boolean b) {
+    protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(@NotNull RandomSource randomSource, boolean b) {
         return null;
     }
 
     @Override
-    public boolean growTree(ServerLevel $$0, ChunkGenerator $$1, BlockPos $$2, BlockState $$3, RandomSource $$4) {
+    public boolean growTree(@NotNull ServerLevel $$0, @NotNull ChunkGenerator $$1, @NotNull BlockPos $$2, @NotNull BlockState $$3, @NotNull RandomSource $$4) {
         return false;
     }
 }
